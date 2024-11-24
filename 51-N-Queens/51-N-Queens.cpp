e7a3a97b0fa3d5170923c383859a685f457a6d49
Matthew Lim <matthewlimcenterdiv@gmail.com>
2024-11-24 19:26:06 +1100

    Create file

---
+++ 51-N-Queens/51-N-Queens.cpp
@@ -0,0 +1,108 @@
+class Solution {
+public:
+    void diagonal_processing(vector<vector<int>>& curr_available, int n, int y, int x, int y_dir, int x_dir, int delta) {
+        int curr_y = y;
+        int curr_x = x;
+        while (curr_y < n &&
+                curr_y >= 0 && 
+                curr_x < n 
+                && curr_x >= 0
+        ) {
+            curr_available[curr_y][curr_x] += delta;
+            curr_y += y_dir;
+            curr_x += x_dir;
+        }
+    }
+
+    void increment_hits(vector<vector<int>>& curr_available, int n, int y, int x) {
+        // Increment hits on row, col, and diagonal
+        // Row and col
+        for (int i = 0; i < n; i++) {
+            curr_available[y][i]++;
+            curr_available[i][x]++;
+        }
+
+        // Diagonals
+        diagonal_processing(curr_available, n, y, x, 1, 1, 1);
+        diagonal_processing(curr_available, n, y, x, -1, 1, 1);
+        diagonal_processing(curr_available, n, y, x, 1, -1, 1);
+        diagonal_processing(curr_available, n, y, x, -1, -1, 1);
+
+        // The current square was counted 6 times.
+        curr_available[y][x] -= 5;
+    }
+
+    void decrement_hits(vector<vector<int>>& curr_available, int n, int y, int x) {
+        // Increment hits on row, col, and diagonal
+        // Row and col
+        for (int i = 0; i < n; i++) {
+            curr_available[y][i]--;
+            curr_available[i][x]--;
+        }
+
+        // Diagonals
+        diagonal_processing(curr_available, n, y, x, 1, 1, -1);
+        diagonal_processing(curr_available, n, y, x, -1, 1, -1);
+        diagonal_processing(curr_available, n, y, x, 1, -1, -1);
+        diagonal_processing(curr_available, n, y, x, -1, -1, -1);
+
+        // The current square was counted 6 times.
+        curr_available[y][x] += 5;
+    }
+
+    void backtrack(vector<vector<string>>& res, 
+                    vector<vector<int>>& curr_available, 
+                    vector<string>& curr_res, 
+                    int n, 
+                    int queen_count,
+                    int curr_y,
+                    int curr_x
+    ) {
+        if (queen_count == n) {
+            res.push_back(curr_res);
+            return;
+        }
+
+        for (int i = curr_y; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (curr_available[i][j] > 0) {
+                    continue;
+                }
+                curr_res[i][j] = 'Q';
+                increment_hits(curr_available, n, i, j);
+                backtrack(res, curr_available, curr_res, n, queen_count + 1, i, j);
+                decrement_hits(curr_available, n, i, j);
+                curr_res[i][j] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        // Restrictions:
+        // No 2 queens on same row. 
+        // No 2 queens on same column.
+        // No 2 queens on same diagonal. 
+        vector<vector<string>> res;
+        // The current board with just the Queens and '.'
+        vector<string> curr_res(n, string(n, '.'));
+        // The current board availabilities, where 'x' is for the squares that can't be occupied. 
+        vector<vector<int>> curr_available(n, vector<int>(n, 0));
+        // vector<vector<int>> curr_available(10, vector<int>(10, 0));
+
+        int queen_count = 0;
+
+        backtrack(res, curr_available, curr_res, n, queen_count, 0, 0);
+
+        // increment_hits(curr_available, 10, 3, 4);
+        // for (vector<int> vec : curr_available) {
+        //     for (int cell : vec) {
+        //         cout << cell;
+        //     }
+
+        //     cout << endl;
+        // } 
+        // decrement_hits(curr_available, 10, 3, 4);
+
+        return res;
+    }
+};
