2ab693c617cf6c49d2d553a13c720d9c25431097
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-01-16 01:01:54 +1100

    Update file 1/16/2025, 1:01:51 AM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // Easy using a cpp ordered map. An ordered map in cpp is ordered by key in increasing order.
+        // Using a monotonic queue
         vector<int> res;
+        // val : index
+        deque<pair<int, int>> mono_q;
+
         int l_ptr = 0;
-        map<int, int> nums_count;
+
         for (int r_ptr = 0; r_ptr < nums.size(); r_ptr++) {
-            nums_count[nums[r_ptr]]++;
+            while (!mono_q.empty() && mono_q.back().first < nums[r_ptr]) {
+                mono_q.pop_back();
+            }
+            mono_q.push_back({ nums[r_ptr], r_ptr });
+
             if (r_ptr - l_ptr + 1 == k) {
-                res.push_back(nums_count.rbegin()->first);
-                nums_count[nums[l_ptr]]--;
-                if (nums_count[nums[l_ptr]] == 0) nums_count.erase(nums[l_ptr]);
+                while (!mono_q.empty() && mono_q.front().second <= r_ptr - k) {
+                    mono_q.pop_front();
+                }
+                res.push_back(mono_q.front().first);
                 l_ptr++;
             }
         }
