7f1473f5fb57adfd7e5b6e2778e9fc5d3c9ddb5f
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-02-25 20:12:26 +1100

    File updated at 2/25/2025, 8:12:25 PM

--- 280-WiggleSort/280-WiggleSort.cpp
+++ 280-WiggleSort/280-WiggleSort.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        deque<int> dq;
-        for (int num : nums) {
-            dq.push_back(num);
-        }
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size() - 1; i++) {
             if (i % 2 == 0) {
-                nums[i] = dq.front();
-                dq.pop_front();      
+                if (nums[i] > nums[i + 1]) {
+                    swap(nums[i], nums[i + 1]);
+                }
             } else {
-                nums[i] = dq.back();
-                dq.pop_back();
+                if (nums[i] < nums[i + 1]) {
+                    swap(nums[i], nums[i + 1]);
+                }
             }
         }
     }
