b682a0ca87af25916b0124ef77113dca29196f01
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-02-26 21:35:06 +1100

    File updated at 2/26/2025, 9:35:06 PM

--- 1570-DotProductofTwoSparseVectors/1570-DotProductofTwoSparseVectors.cpp
+++ 1570-DotProductofTwoSparseVectors/1570-DotProductofTwoSparseVectors.cpp
@@ -1,23 +1,16 @@
 class SparseVector {
-public:
-    vector<int> v;
     
-    SparseVector(vector<int> &nums) {
-        v = nums;
+    int[] nums;
+    SparseVector(int[] nums) {
+        this.nums = nums;
     }
-    
-    // Return the dotProduct of two sparse vectors
-    int dotProduct(SparseVector& vec) {
-        int res = 0;
-        for (int i = 0; i < vec.v.size(); i++) {
-            res += v[i] * vec.v[i];
-        }
-
-        return res;
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {        
+        int n = nums.length;
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            sum += nums[i] * vec.nums[i];
+        }        
+        return sum;
     }
-};
-
-// Your SparseVector object will be instantiated and called as such:
-// SparseVector v1(nums1);
-// SparseVector v2(nums2);
-// int ans = v1.dotProduct(v2);
+}
