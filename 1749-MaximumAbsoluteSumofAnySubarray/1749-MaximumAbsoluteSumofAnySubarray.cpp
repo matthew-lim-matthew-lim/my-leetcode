051f296404827080d83f3bbf832437d47dd962c8
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-02-26 13:46:21 +1100

    Create file

---
+++ 1749-MaximumAbsoluteSumofAnySubarray/1749-MaximumAbsoluteSumofAnySubarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        // Prefix sum, and then find the largest and smallest values. 
+        int n = nums.size();
+        vector<int> prefix(n + 1, 0);
+
+        for (int i = 1; i < n + 1; i++) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+            cout << prefix[i] << endl;
+        }
+
+        int maxVal = *max_element(prefix.begin(), prefix.end());
+        int minVal = *min_element(prefix.begin(), prefix.end());
+
+        return max(abs(minVal - maxVal), maxVal - minVal);
+    }
+};
