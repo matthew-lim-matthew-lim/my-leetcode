a1be9d36047b1e4aa72ca964df30eee2a84f2286
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-02-28 17:10:42 +1100

    File updated at 2/28/2025, 5:10:40 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>>dp(text1.size(), vector<int>(text2.size(), 0));
-        for (int i = 0; i < text1.size(); i++) {
-            for (int j = 0; j < text2.size(); j++) {
-                if (text1[i] == text2[j]) {
-                    int top_left = i - 1 >= 0 && j - 1 >= 0 ? dp[i - 1][j - 1] : 0;
-                    dp[i][j] = top_left + 1;
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            // Each letter in text1
+            for (int j = 1; j <= m; j++) {
+                // Each letter in text2
+                if (text1[i - 1] == text2[j - 1]) {
+                    // add a new letter to the LCS at dp[i][j].
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    int top = i - 1 >= 0 ? dp[i - 1][j] : 0;
-                    int left = j - 1 >= 0 ? dp[i][j - 1] : 0;
-                    dp[i][j] = max(top, left);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        return dp[text1.size() - 1][text2.size() - 1];
+        // for (int i = 1; i <= n; i++) {
+        //     // Each letter in text1
+        //     for (int j = 1; j <= m; j++) {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+        return dp[n][m];
     }
 };
