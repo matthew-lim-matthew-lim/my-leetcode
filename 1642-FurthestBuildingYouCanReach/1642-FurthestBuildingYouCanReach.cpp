54a50d96df8dc7b01a1f09e5e23e0f5525a5330e
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-03-05 14:12:55 +1100

    Create file

---
+++ 1642-FurthestBuildingYouCanReach/1642-FurthestBuildingYouCanReach.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+
+        priority_queue<int> pq;
+        for (int i = 0; i < heights.size() - 1; i++) {
+            int bricksRequired = heights[i + 1] - heights[i];
+
+            if (bricksRequired <= 0) {
+                continue;
+            }
+
+            pq.push(bricksRequired);
+            bricks -= bricksRequired;
+
+            if (bricks < 0 && ladders == 0) {
+                return i;
+            }
+
+            if (bricks < 0) {
+                bricks += pq.top();
+                pq.pop();
+                ladders--;
+            }
+        }
+
+        return heights.size() - 1;
+    }
+};
