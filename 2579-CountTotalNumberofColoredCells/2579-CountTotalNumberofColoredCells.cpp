d85502eb89d9aefd922e762f94dfad0c1056d466
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-03-05 11:35:57 +1100

    File updated at 3/5/2025, 11:35:56 AM

--- 2579-CountTotalNumberofColoredCells/2579-CountTotalNumberofColoredCells.cpp
+++ 2579-CountTotalNumberofColoredCells/2579-CountTotalNumberofColoredCells.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        // += 4 * (n - 1)
-        if (n == 1) return 1;
-        return 4 * (n - 1) + coloredCells(n - 1);
+        // Math and DP
+        long long res = 1;
+        for (int i = 2; i <= n; i++) {
+            res += 4 * (i - 1);
+        }
+        return res;
     }
 };
