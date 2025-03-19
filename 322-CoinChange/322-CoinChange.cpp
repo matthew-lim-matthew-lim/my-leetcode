af775aab3b5c8a18481f338aa56adc92a145c7f7
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-03-19 16:21:35 +1100

    File updated at 3/19/2025, 4:21:34 PM

--- 322-CoinChange/322-CoinChange.cpp
+++ 322-CoinChange/322-CoinChange.cpp
@@ -3,18 +3,20 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         // Dynamic Programming
         // dp[value] = number of coins used. 
+
+        // Bottom-up
         vector<int> dp(amount + 1, amount + 1);
-        dp[amount] = 0;
-        for (int i = amount; i >= 0; i--) {
+        dp[0] = 0;
+        for (int i = 0; i <= amount; i++) {
             // Can we make that amount with less coins?
             for (long long coin : coins) {
-                if (i + coin >= amount + 1) {
+                if (i - coin < 0) {
                     continue;
                 }
-                dp[i] = min(dp[i + coin] + 1, dp[i]);
+                dp[i] = min(dp[i - coin] + 1, dp[i]);
             }
         }
 
-        return dp[0] == amount + 1 ? -1 : dp[0];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 };
