3e087ae5b2ddb7d99b4407cd97a889f9ee731962
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-03-24 11:37:51 +1100

    File updated at 3/24/2025, 11:37:50 AM

--- 3169-CountDaysWithoutMeetings/3169-CountDaysWithoutMeetings.cpp
+++ 3169-CountDaysWithoutMeetings/3169-CountDaysWithoutMeetings.cpp
@@ -1,4 +1,4 @@
-// Last updated: 3/24/2025, 11:37:38 AM
+// Last updated: 3/24/2025, 11:37:50 AM
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
@@ -9,19 +9,12 @@ public:
         }
         sort(events.begin(), events.end());
 
-
-        // for (vector<int> &event : events) {
-        //     cout << event[0] << " " << event[1] << endl;
-        // }
-
         int currMeetings = 0;
         int res = 0;
         for (int i = 0; i < events.size(); i++) {
-            // cout << currMeetings << endl;
             if (currMeetings == 0) {
                 int prevDay = (i == 0) ? 0 : events[i - 1][0];
                 res += events[i][0] - prevDay - 1;
-                // cout << events[i][0] << " prevDay: " << prevDay << endl;
             }
             if (events[i][1] == 0) {
                 currMeetings++;
@@ -32,7 +25,6 @@ public:
         if (currMeetings == 0 && events.back()[0] != days) {
             int prevDay = events.back()[0];
             res += days - prevDay;
-            // cout << days << " prevDay: " << prevDay << endl;
         }
 
         return res;
