d59a39a490024bb04c11b3ecca642cd7a3bade60
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-05 15:47:21 +1100

    Create file

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,24 @@
+// Last updated: 4/5/2025, 3:47:22 PM
+class Solution {
+public:
+    int climbStairs(int n) {
+        // Bottom-up DP.
+        vector<int> dp(n + 1, 0);
+        // At dp[0], we are at step 0. There is 1 way to get to dp[0]. 
+        dp[0] = 1;
+        for (int i = 0; i < n + 1; i++) {
+            if (i + 2 < n + 1) {
+                dp[i + 2] += dp[i];
+            }
+            if (i + 1 < n + 1) {
+                dp[i + 1] += dp[i];
+            }
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            cout << dp[i] << endl;
+        }
+
+        return dp[n];
+    }
+};
