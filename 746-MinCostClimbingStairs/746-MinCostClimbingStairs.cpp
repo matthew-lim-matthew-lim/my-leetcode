64c6e4107c4ae61125c9589ef275cd2af1a40870
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-05 16:01:00 +1100

    File updated at 4/5/2025, 4:01:00 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.cpp
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.cpp
@@ -1,19 +1,17 @@
+// Last updated: 4/5/2025, 4:01:00 PM
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        // Solve opt(i) which is the min cost to climb to 'i'.
-        // opt(i) = min(opt(i - 1) + opt[i - 1], opt(i - 2) + opt[i - 2]);
-
-        vector<int> dp(cost.size() + 1, INT_MAX);
-
-        // Solve in bottom-up order.
-        dp[0] = 0;
-        dp[1] = 0;
-
-        for (int i = 2; i < cost.size() + 1; i++) {
-            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        // Bottom up dp
+        // Cheapest cost to get to the ith staircase.
+        int n = cost.size();
+        vector<int> dp(n, INT_MAX);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
         }
 
-        return dp[cost.size()];
+        return min(dp[n - 1], dp[n - 2]);
     }
 };
