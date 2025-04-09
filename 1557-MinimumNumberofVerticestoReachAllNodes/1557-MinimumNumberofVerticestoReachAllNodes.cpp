2bb41ed37d2fca2e9f9bcb1c21620155dad87faf
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-09 23:27:37 +1000

    File updated at 4/9/2025, 11:27:37 PM

--- 1557-MinimumNumberofVerticestoReachAllNodes/1557-MinimumNumberofVerticestoReachAllNodes.cpp
+++ 1557-MinimumNumberofVerticestoReachAllNodes/1557-MinimumNumberofVerticestoReachAllNodes.cpp
@@ -1,15 +1,16 @@
+// Last updated: 4/9/2025, 11:27:37 PM
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        // All the vertices that have no incoming edges.
-        vector<int> incomingEdges(n, 0);
+        // Just create a set with all nodes, and exclude nodes that don't have an incoming edge
+        vector<bool> incoming(n, false);
         for (vector<int> &edge : edges) {
-            incomingEdges[edge[1]]++;
+            incoming[edge[1]] = true;
         }
 
         vector<int> res;
         for (int i = 0; i < n; i++) {
-            if (incomingEdges[i] == 0) {
+            if (incoming[i] == false) {
                 res.push_back(i);
             }
         }
