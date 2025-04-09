573a80592b0ad45828b97d12d3b25d45afb9428b
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-09 13:10:28 +1000

    File updated at 4/9/2025, 1:10:28 PM

--- 213-HouseRobberII/213-HouseRobberII.cpp
+++ 213-HouseRobberII/213-HouseRobberII.cpp
@@ -1,44 +1,35 @@
+// Last updated: 4/9/2025, 1:10:28 PM
 class Solution {
 public:
-    int rob_house_helper(vector<int>& nums) {
-        if (nums.size() == 0) {
-            return 0;
-        }
-
-        vector<int> dp(nums.size(), 0);
-
-        if (nums.size() >= 1) dp[0] = nums[0];
-        if (nums.size() >= 2) dp[1] = max(nums[1], nums[0]);
-
-        for (int i = 2; i < nums.size(); i++) {
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-
-        return dp[nums.size() - 1];
-    }
-
     int rob(vector<int>& nums) {
-        // Calculate the max returns when 0 is taken
-        vector<int> nums_copy;
-        for (int i = 0; i < nums.size(); i++) {
-            // Exclude the first number, and the numbers adjacent to it.
-            if (i == 0 || i == 1 || i == nums.size() - 1) {
-                continue;
-            }
-            nums_copy.push_back(nums[i]);
+        // dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        // Break it into 2 cases
+        int n = nums.size();
+        if (n <= 2) {
+            return *max_element(nums.begin(), nums.end());
         }
-        int yes_took_0 = nums[0] + rob_house_helper(nums_copy);
+        vector<vector<int>> dp(2, vector<int>(n, 0));
+        dp[0][0] = nums[0];
+        dp[0][1] = max(nums[0], nums[1]);
+        dp[1][0] = 0;
+        dp[1][1] = nums[1];
 
-        // Calculate the max returns when 0 is not taken
-        nums_copy.clear();
-        for (int i = 0; i < nums.size(); i++) {
-            if (i == 0) {
-                continue;
-            }
-            nums_copy.push_back(nums[i]);
+        for (int i = 2; i < n - 1; i++) {
+            dp[0][i] = max(dp[0][i - 1], dp[0][i - 2] + nums[i]);
+            dp[1][i] = max(dp[1][i - 1], dp[1][i - 2] + nums[i]);
         }
-        int no_took_0 = rob_house_helper(nums_copy);
+        dp[0][n - 1] = dp[0][n - 2];
+        dp[1][n - 1] = max(dp[1][n - 2], dp[1][n - 3] + nums[n - 1]);
 
-        return max(yes_took_0, no_took_0);
+        // cout << "dp[0]:" << endl;
+        // for (int i = 0; i < n; i++) {
+        //     cout << dp[0][i] << endl;
+        // }
+        // cout << "dp[1]:" << endl;
+        // for (int i = 0; i < n; i++) {
+        //     cout << dp[1][i] << endl;
+        // }
+
+        return max(dp[0][n - 1], dp[1][n - 1]);
     }
 };
