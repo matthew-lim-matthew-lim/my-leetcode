3eccb90558dc395137b256e2f2a7f0ed12e2a9ac
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-09 13:52:19 +1000

    File updated at 4/9/2025, 1:52:18 PM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,19 +1,20 @@
+// Last updated: 4/9/2025, 1:52:18 PM
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // At any point, the robot can move either down or right. 
-        // opt(i, j) is the number of possible unique paths to get to point (i, j).
-        // opt(i, j) = opt(i - 1, j) + opt(i, j - 1);
-        // opt(0, 0) = 1;
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) continue;
-                int up = i - 1 >= 0 ? dp[i - 1][j] : 0;
-                int left = j - 1 >= 0 ? dp[i][j - 1] : 0;
-                dp[i][j] = up + left;
+        // dp[i][j] represents the number of ways to get to (i, j). 
+        // dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        dp[1][1] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
-        } 
-        return dp[m - 1][n - 1];
+        }
+
+        return dp[n][m];
     }
 };
