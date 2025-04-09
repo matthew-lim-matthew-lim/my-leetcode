da1cfb277c4039951ee04001214b7e193cbf6f5b
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-09 14:43:42 +1000

    Create file

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.cpp
@@ -0,0 +1,69 @@
+// Last updated: 4/9/2025, 2:43:40 PM
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        // To pass TLE, maybe need to keep track of smallest positive val, and largest negative val (values closest to 0).
+        // If number positive, use the smallest > 0.
+        // If number negative, use the largest < 0.
+        // This should make it O(n), if I use an unordered_set to track zeros
+
+        // DP
+        // Prefix sum tyshi 
+        // In the prefix sum, if there is a 0, treat the next value as a 1. 
+        // But keep track of the location of the 0s. 
+        int n = nums.size();
+        vector<int> prefixProd(n, 1);
+        unordered_set<int> zeroIndex;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                zeroIndex.insert(i);
+                prefixProd[i] = 1;
+                continue;
+            }
+            prefixProd[i] = nums[i];
+            if (i > 0) {
+                prefixProd[i] = prefixProd[i - 1] * nums[i];
+            }
+        }
+
+        // O(n)
+        int res = nums.front();
+
+        // Now, go through the potential subarrays.
+        int smallestAboveZero = INT_MAX;
+        int largestBelowZero = INT_MIN;
+        // If we encounter a zero, then reset the values.
+
+        // for (int i = 0; i < n; i++) {
+        //     cout << prefixProd[i] << endl;
+        // }
+
+        // i is the last number in the subarray
+        bool encounteredZero = false;
+        for (int i = 0; i < n; i++) {
+            res = max(res, nums[i]);
+
+            if (zeroIndex.contains(i)) {
+                smallestAboveZero = INT_MAX;
+                largestBelowZero = INT_MIN;
+                encounteredZero = true;
+                continue;
+            }
+
+            if (!encounteredZero) {
+                res = max(res, prefixProd[i]);
+            }
+
+            if (prefixProd[i] > 0) {
+                // if (smallestAboveZero != INT_MAX) res = max(res, prefixProd[i] / smallestAboveZero);
+                res = max(res, prefixProd[i]);
+            }
+            if (prefixProd[i] < 0) {
+                if (largestBelowZero != INT_MIN) res = max(res, prefixProd[i] / largestBelowZero);
+                largestBelowZero = max(largestBelowZero, prefixProd[i]);
+            }
+        }
+
+        return res;
+    }
+};
