7b5c86a377d59edd8f9aa4030614aec8dbf3da6d
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-10 18:30:24 +1000

    File updated at 4/10/2025, 6:30:22 PM

--- 279-PerfectSquares/279-PerfectSquares.cpp
+++ 279-PerfectSquares/279-PerfectSquares.cpp
@@ -1,16 +1,7 @@
-// Last updated: 4/10/2025, 6:30:11 PM
+// Last updated: 4/10/2025, 6:30:22 PM
 class Solution {
 public:
     int numSquares(int n) {
-        // Consider the numbers we can make with perfect squares. 
-        // Keep track of the numbers we can make with perfect squares, and go down the list to make the current number.
-        // vector<int> perfSquares;
-        // for (int i = 1; i < n; i++) {
-        //     if (pow((int)sqrt(i), 2) == i) {
-        //         perfSquares.push_back(i);
-        //     }
-        // }
-
         // We can find how many numbers it takes to make each number
         // dp[i] = min(dp[i], dp[i - j] + dp[j]);
         vector<int> dp(n + 1, 0);
@@ -25,10 +16,6 @@ public:
                 dp[i] = min(dp[i], dp[i - j] + dp[j]);
             }
         }
-
-        // for (int i = 1; i <= n; i++) {
-        //     cout << dp[i] << endl;
-        // }
 
         return dp[n];
     }
