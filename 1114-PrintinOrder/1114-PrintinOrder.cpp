b4cb079f0865ac83ff6c1db84c3763147020a7fa
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-21 21:07:40 +1000

    File updated at 4/21/2025, 9:07:40 PM

--- 1114-PrintinOrder/1114-PrintinOrder.cpp
+++ 1114-PrintinOrder/1114-PrintinOrder.cpp
@@ -1,53 +1,49 @@
-// Last updated: 4/21/2025, 8:56:17 PM
+// Last updated: 4/21/2025, 9:07:40 PM
 class Foo {
+// CV might be good performance since blocking
 private:
     std::mutex mtx;
-    std::atomic<int> curr = 0; 
+    std::condition_variable cv;
+    std::atomic<int> curr = 0;
+
 public:
-// Atomics is the clear way to do this
     Foo() {
         
     }
 
     void first(function<void()> printFirst) {
-        while (true) {
-            std::lock_guard<std::mutex> lock(mtx);
-            curr.load(std::memory_order_acquire);
-            if (curr != 0) {
-                continue;
-            }
-            // printFirst() outputs "first". Do not change or remove this line.
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [&] {
+                return curr.load(std::memory_order_acquire) == 0;
+            });
             printFirst();
             curr.fetch_add(1, std::memory_order_release);
-            return;
         }
+        cv.notify_all();
     }
 
     void second(function<void()> printSecond) {
-        while (true) {
-            std::lock_guard<std::mutex> lock(mtx);
-            curr.load(std::memory_order_acquire);
-            if (curr != 1) {
-                continue;
-            }
-            // printSecond() outputs "second". Do not change or remove this line.
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [&] {
+                return curr.load(std::memory_order_acquire) == 1;
+            });
             printSecond();
             curr.fetch_add(1, std::memory_order_release);
-            return;
         }
+        cv.notify_all();
     }
 
     void third(function<void()> printThird) {
-        while (true) {
-            std::lock_guard<std::mutex> lock(mtx);
-            curr.load(std::memory_order_acquire);
-            if (curr != 2) {
-                continue;
-            }
-            // printThird() outputs "third". Do not change or remove this line.
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [&] {
+                return curr.load(std::memory_order_acquire) == 2;
+            });
             printThird();
             curr.fetch_add(1, std::memory_order_release);
-            return;
         }
+        cv.notify_all();
     }
 };
