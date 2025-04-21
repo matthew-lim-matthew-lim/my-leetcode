7b06cde6c60522d485b37280992c560f693b79bd
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-21 21:35:45 +1000

    Create file

---
+++ 1115-PrintFooBarAlternately/1115-PrintFooBarAlternately.cpp
@@ -0,0 +1,32 @@
+// Last updated: 4/21/2025, 9:35:45 PM
+class FooBar {
+private:
+    int n;
+    std::binary_semaphore fooReady{1};
+    std::binary_semaphore barReady{0};
+
+public:
+    FooBar(int n) {
+        this->n = n;
+    }
+
+    void foo(function<void()> printFoo) {
+        
+        for (int i = 0; i < n; i++) {
+            fooReady.acquire();
+        	// printFoo() outputs "foo". Do not change or remove this line.
+        	printFoo();
+            barReady.release();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+            barReady.acquire();
+        	// printBar() outputs "bar". Do not change or remove this line.
+        	printBar();
+            fooReady.release();
+        }
+    }
+};
