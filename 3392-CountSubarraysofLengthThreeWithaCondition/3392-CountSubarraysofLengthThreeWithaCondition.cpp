ffbd0dc2a11284115e0945dcc80645063ec3ccd6
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-04-27 13:10:50 +1000

    File updated at 4/27/2025, 1:10:48 PM

--- 3392-CountSubarraysofLengthThreeWithaCondition/3392-CountSubarraysofLengthThreeWithaCondition.cpp
+++ 3392-CountSubarraysofLengthThreeWithaCondition/3392-CountSubarraysofLengthThreeWithaCondition.cpp
@@ -1,11 +1,11 @@
-// Last updated: 4/27/2025, 1:08:07 PM
+// Last updated: 4/27/2025, 1:10:48 PM
 class Solution {
 public:
     int countSubarrays(vector<int>& nums) {
         int n = nums.size();
         int res = 0;
         for (int r = 2; r < n; r++) {
-            res += (nums[r - 1] / 2.0) == (nums[r - 2] + nums[r]);
+            res += nums[r - 1] == (nums[r - 2] + nums[r]) * 2;
         }
 
         return res;
