cb432acdf90576ed9e2757a04a5a25c8d708f4bd
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-05-27 22:20:24 +1000

    Create 628-MaximumProductofThreeNumbers

---
+++ 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.cpp
@@ -0,0 +1,13 @@
+// Last updated: 5/27/2025, 10:20:22 PM
+class Solution {
+public:
+/* 
+Sort it.
+ */
+
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); 
+        int n = nums.size();
+        return max(nums[0] * nums[1] * nums[n-1], nums[n-3] * nums[n-2] * nums[n-1]);
+    }
+};
