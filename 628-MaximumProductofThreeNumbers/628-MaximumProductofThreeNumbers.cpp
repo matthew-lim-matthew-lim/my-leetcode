a6884e87f747cb0fd0902d5ad314ac3229070606
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-05-27 22:31:23 +1000

    File updated at 5/27/2025, 10:31:21 PM

--- 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.cpp
+++ 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/27/2025, 10:21:29 PM
+// Last updated: 5/27/2025, 10:31:21 PM
 class Solution {
 public:
 /* 
@@ -6,8 +6,32 @@ Sort it.
  */
 
     int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); 
-        int n = nums.size();
-        return max(nums[0] * nums[1] * nums[n-1], nums[n-3] * nums[n-2] * nums[n-1]);
+        priority_queue<int, vector<int>, std::greater<int>> pq1;
+        priority_queue<int> pq2;
+
+        for (int num : nums) {
+            if (pq1.size() >= 3 && num > pq1.top()) {
+                pq1.pop();
+            }
+            if (pq2.size() >= 2 && num < pq2.top()) {
+                pq2.pop();
+            }
+            if (pq1.size() < 3) pq1.push(num);
+            if (pq2.size() < 2) pq2.push(num);
+        }
+
+        int res1 = pq2.top();
+        pq2.pop();
+        res1 *= pq2.top();
+        pq2.pop();
+        int res2 = pq1.top();
+        pq1.pop();
+        res2 *= pq1.top();
+        pq1.pop();
+        res2 *= pq1.top();
+        res1 *= pq1.top();
+        pq1.pop();
+
+        return max(res1, res2);
     }
 };
