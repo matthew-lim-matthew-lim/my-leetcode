066bdbd18076511a9d182286cea5a3a2547c6571
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-06-04 23:55:28 +1000

    Create 3403-FindtheLexicographicallyLargestStringFromtheBoxI

---
+++ 3403-FindtheLexicographicallyLargestStringFromtheBoxI/3403-FindtheLexicographicallyLargestStringFromtheBoxI.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/4/2025, 11:55:26 PM
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        int n = word.size();
+        string res;
+        for (int i = 0; i < n; i++) {
+            res = max(res, word.substr(i, min(n - numFriends + 1, n - i)));
+        }
+        return res;
+    }
+};
