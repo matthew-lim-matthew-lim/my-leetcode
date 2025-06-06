36ba5e5115bfd6bc08d71cca1155c56a2cba2408
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-06-06 22:14:39 +1000

    Create 2434-UsingaRobottoPrinttheLexicographicallySmallestString

---
+++ 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.cpp
@@ -0,0 +1,27 @@
+// Last updated: 6/6/2025, 10:14:36 PM
+class Solution {
+public:
+    string robotWithString(string s) {
+        unordered_map<char, int> cnt;
+        for (char c : s) {
+            cnt[c]++;
+        }
+
+        stack<char> stk;
+        string res;
+        char minCharacter = 'a';
+        for (char c : s) {
+            stk.emplace(c);
+            cnt[c]--;
+            while (minCharacter != 'z' && cnt[minCharacter] == 0) {
+                minCharacter++;
+            }
+            while (!stk.empty() && stk.top() <= minCharacter) {
+                res.push_back(stk.top());
+                stk.pop();
+            }
+        }
+
+        return res;
+    }
+};
