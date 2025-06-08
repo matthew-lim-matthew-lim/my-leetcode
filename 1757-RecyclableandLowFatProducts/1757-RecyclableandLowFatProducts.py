bf38b169a4a24a9d5e0b04c827be114ccceda55f
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-06-08 16:24:26 +1000

    Create 1757-RecyclableandLowFatProducts

---
+++ 1757-RecyclableandLowFatProducts/1757-RecyclableandLowFatProducts.py
@@ -0,0 +1,5 @@
+# Last updated: 6/8/2025, 4:24:24 PM
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')][['product_id']]
