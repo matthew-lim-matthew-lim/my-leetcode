03050bc5a58e0d416157540b8b73cd4374c07dc3
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-06-10 23:35:16 +1000

    Create 3442-MaximumDifferenceBetweenEvenandOddFrequencyI

---
+++ 3442-MaximumDifferenceBetweenEvenandOddFrequencyI/3442-MaximumDifferenceBetweenEvenandOddFrequencyI.py
@@ -0,0 +1,7 @@
+# Last updated: 6/10/2025, 11:35:14 PM
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        c = Counter(s)
+        maxOdd = max(x for x in c.values() if x % 2 == 1)
+        minEven = min(x for x in c.values() if x % 2 == 0)
+        return maxOdd - minEven
