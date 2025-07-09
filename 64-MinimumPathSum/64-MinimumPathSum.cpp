6cf39a3749f244b94fbba7eef23a62e950602aee
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-07-09 22:43:10 +1000

    File updated at 7/9/2025, 10:43:08 PM

--- 64-MinimumPathSum/64-MinimumPathSum.cpp
+++ 64-MinimumPathSum/64-MinimumPathSum.cpp
@@ -1,21 +1,30 @@
+// Last updated: 7/9/2025, 10:43:08 PM
+/* 
+Dynamic programming
+ */
+
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        // opt(i, j) is the minimum path sum to get to (i, j).
-        // opt(i, j) = min(opt(i - 1, j), opt(i, j - 1)) + grid[i][j];
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), INT_MAX));
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (i == 0 && j == 0) {
                     dp[i][j] = grid[i][j];
                     continue;
                 }
-                int top = i - 1 >= 0 ? dp[i - 1][j] : INT_MAX;
-                int left = j - 1 >= 0 ? dp[i][j - 1] : INT_MAX;
-                dp[i][j] = min(top, left) + grid[i][j];
+
+                int up = i == 0 ? dp[i][j - 1] : dp[i - 1][j];
+                int left = j == 0 ? dp[i - 1][j] : dp[i][j - 1];
+
+                dp[i][j] = min(up, left) + grid[i][j];
             }
         }
-        
-        return dp[grid.size() - 1][grid[0].size() - 1];
+
+        return dp[n - 1][m - 1];
     }
 };
