8b872937bb6f3f5ea13ec9d5278ee5623e0f377c
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-07-09 20:50:44 +1000

    File updated at 7/9/2025, 8:50:42 PM

--- 54-SpiralMatrix/54-SpiralMatrix.cpp
+++ 54-SpiralMatrix/54-SpiralMatrix.cpp
@@ -1,32 +1,43 @@
+// Last updated: 7/9/2025, 8:50:42 PM
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        vector<vector<int>> direction = { {0, 1}, {-1, 0}, {0, -1}, {1, 0} };
+
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
 
-        vector<int> currPos = { 0, 0 };
-        vector<int> order;
-        
-        vector<vector<int>> direction = {{ 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }};
-        int d = 0;
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+
+        vector<int> pos = {0, 0};
+        int currD = 0;
+
         while (true) {
-            order.push_back(matrix[currPos[0]][currPos[1]]);
-            visited[currPos[0]][currPos[1]] = true;
-            
-            vector<int> newPos = { currPos[0] + direction[d][0], currPos[1] + direction[d][1] };
-            if (newPos[0] < n && newPos[1] < m && newPos[0] >= 0 && newPos[1] >= 0 && visited[newPos[0]][newPos[1]] == false) {
-                currPos = newPos;
-            } else {
-                d = (d + 1) % 4;
-                vector<int> newPos = { currPos[0] + direction[d][0], currPos[1] + direction[d][1] };
-                if (!(newPos[0] < n && newPos[1] < m && newPos[0] >= 0 && newPos[1] >= 0 && visited[newPos[0]][newPos[1]] == false)) {
-                    return order;
-                }
-                currPos = newPos;
+            if (visited[pos[0]][pos[1]] == 0) {
+                res.push_back(matrix[pos[0]][pos[1]]);
+            }
+            // cout << pos[0] << ", " << pos[1] << " " << visited[pos[0]][pos[1]] << endl;
+            visited[pos[0]][pos[1]]++;
+            if (pos[0] + direction[currD][0] < n && 
+                pos[0] + direction[currD][0] >= 0 &&
+                pos[1] + direction[currD][1] < m &&
+                pos[1] + direction[currD][1] >= 0 &&
+                visited[pos[0] + direction[currD][0]][pos[1] + direction[currD][1]] == 0
+            ) {
+                pos[0] += direction[currD][0];
+                pos[1] += direction[currD][1];
+            }    
+            else {
+                currD++;
+                currD %= 4;
+            }
+
+            if (visited[pos[0]][pos[1]] > 4) {
+                return res;
             }
         }
 
-        return order;
+        return res;
     }
 };
