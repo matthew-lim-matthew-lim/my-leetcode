5587ca4d60dc9ff8ad758f451d9b85c37ebac6bd
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-07-10 15:26:03 +1000

    File updated at 7/10/2025, 3:26:03 PM

--- 934-ShortestBridge/934-ShortestBridge.cpp
+++ 934-ShortestBridge/934-ShortestBridge.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/10/2025, 3:23:30 PM
+// Last updated: 7/10/2025, 3:26:03 PM
 /* 
 BFS from the borders of an island. 
 
@@ -14,89 +14,78 @@ public:
         vector<vector<int>> island(n, vector<int>(m, 0));
         queue<vector<int>> bridgeQ;
 
-        int currIsland = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1 && island[i][j] == 0) {
                     // This is a new island, currIsland. 
                     queue<vector<int>> q;
                     q.push({ i, j });
-                    island[i][j] = currIsland;
+                    island[i][j] = 1;
 
                     while (!q.empty()) {
                         int y = q.front()[0];
                         int x = q.front()[1];
                         q.pop();
 
-                        if (currIsland == 1) {
-                            bridgeQ.push({ y, x, 0 });
-                        }
+                        bridgeQ.push({ y, x, 0 });
 
                         if (y + 1 < n && island[y + 1][x] == 0 && grid[y + 1][x] == 1) {
-                            island[y + 1][x] = currIsland;
+                            island[y + 1][x] = 1;
                             q.push({ y + 1, x });
                         }
                         if (y - 1 >= 0 && island[y - 1][x] == 0 && grid[y - 1][x] == 1) {
-                            island[y - 1][x] = currIsland;
+                            island[y - 1][x] = 1;
                             q.push({ y - 1, x });
                         }
                         if (x + 1 < m && island[y][x + 1] == 0 && grid[y][x + 1] == 1) {
-                            island[y][x + 1] = currIsland;
+                            island[y][x + 1] = 1;
                             q.push({ y, x + 1 });
                         }
                         if (x - 1 >= 0 && island[y][x - 1] == 0 && grid[y][x - 1] == 1) {
-                            island[y][x - 1] = currIsland;
+                            island[y][x - 1] = 1;
                             q.push({ y, x - 1 });
                         }
                     }
 
-                    currIsland++;
+                    while (!bridgeQ.empty()) {
+                        int y = bridgeQ.front()[0];
+                        int x = bridgeQ.front()[1];
+                        int lvl = bridgeQ.front()[2];
+                        bridgeQ.pop();
+
+                        if (y + 1 < n && island[y + 1][x] != 1) {
+                            if (grid[y + 1][x] == 1) {
+                                return lvl;
+                            }
+                            island[y + 1][x] = 1;
+                            bridgeQ.push({ y + 1, x, lvl + 1 });
+                        }
+                        if (y - 1 >= 0 && island[y - 1][x] != 1) {
+                            if (grid[y - 1][x] == 1) {
+                                return lvl;
+                            }
+                            island[y - 1][x] = 1;
+                            bridgeQ.push({ y - 1, x, lvl + 1 });
+                        }
+                        if (x + 1 < m && island[y][x + 1] != 1) {
+                            if (grid[y][x + 1] == 1) {
+                                return lvl;
+                            }
+                            island[y][x + 1] = 1;
+                            bridgeQ.push({ y, x + 1, lvl + 1 });
+                        }
+                        if (x - 1 >= 0 && island[y][x - 1] != 1) {
+                            if (grid[y][x - 1] == 1) {
+                                return lvl;
+                            }
+                            island[y][x - 1] = 1;
+                            bridgeQ.push({ y, x - 1, lvl + 1 });
+                        }
+                    }
                 }
             }
         }
 
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < m; j++) {
-        //         cout << island[i][j];
-        //     }
-        //     cout << endl;
-        // }
-
-        while (!bridgeQ.empty()) {
-            int y = bridgeQ.front()[0];
-            int x = bridgeQ.front()[1];
-            int lvl = bridgeQ.front()[2];
-            bridgeQ.pop();
-
-            if (y + 1 < n && island[y + 1][x] != 1) {
-                if (island[y + 1][x] == 2) {
-                    return lvl;
-                }
-                island[y + 1][x] = 1;
-                bridgeQ.push({ y + 1, x, lvl + 1 });
-            }
-            if (y - 1 >= 0 && island[y - 1][x] != 1) {
-                if (island[y - 1][x] == 2) {
-                    return lvl;
-                }
-                island[y - 1][x] = 1;
-                bridgeQ.push({ y - 1, x, lvl + 1 });
-            }
-            if (x + 1 < m && island[y][x + 1] != 1) {
-                if (island[y][x + 1] == 2) {
-                    return lvl;
-                }
-                island[y][x + 1] = 1;
-                bridgeQ.push({ y, x + 1, lvl + 1 });
-            }
-            if (x - 1 >= 0 && island[y][x - 1] != 1) {
-                if (island[y][x - 1] == 2) {
-                    return lvl;
-                }
-                island[y][x - 1] = 1;
-                bridgeQ.push({ y, x - 1, lvl + 1 });
-            }
-        }
 
         return 0;
     }
