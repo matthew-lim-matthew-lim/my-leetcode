36e944c037bf6c075a376d14b985ddf59baeb3a2
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-07-10 12:38:27 +1000

    File updated at 7/10/2025, 12:38:26 PM

--- 207-CourseSchedule/207-CourseSchedule.cpp
+++ 207-CourseSchedule/207-CourseSchedule.cpp
@@ -1,50 +1,52 @@
+// Last updated: 7/10/2025, 12:38:26 PM
+/* 
+[a, b] means you need to take b first before a. 
+
+Make a graph. Key is start (b) and is dest (a). 
+While constructing, keep track of nodes that have no dependencies.
+
+BFS traverse graph to see if you can visit all nodes. 
+ */
+
 class Solution {
 public:
-    bool dfs(vector<vector<int>> &adjList, int curr, vector<bool> &recursiveStack, vector<bool> &visited) {
-        // cout << curr << endl;
-        visited[curr] = true;
-        recursiveStack[curr] = true;
-        for (int neigh : adjList[curr]) {
-            if (!visited[neigh]) {
-                if (!dfs(adjList, neigh, recursiveStack, visited)) {
-                    return false;
-                }
-            } else if (recursiveStack[neigh]) {  // Found a back edge (cycle)
-                return false;
-            }
-        }
-
-        recursiveStack[curr] = false;
-        return true;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // Cycle detection, use DFS
-        vector<vector<int>> adjList(numCourses, vector<int>());
-
-
-        for (vector<int> &edge : prerequisites) {
-            if (edge[0] == edge[1]) {
-                return false;
-            }
-            // Reverse the edges
-            adjList[edge[1]].push_back(edge[0]);
-            // We will start at the node that was dst, so leave that one as a valid start node. 
+        unordered_set<int> validStart;
+        for (int i = 0; i < numCourses; i++) {
+            validStart.insert(i);
         }
 
-        // Need to use DFS bruh.
+        unordered_map<int, unordered_set<int>> nodeToMyPrereqs;
+        unordered_map<int, unordered_set<int>> nodeToNieghbours;
 
-        vector<bool> visited(numCourses, false);
-        vector<bool> recursiveStack(numCourses, false);
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (!dfs(adjList, i, recursiveStack, visited)) {
-                    return false;
+        // Construct graph
+        for (const vector<int> &edge : prerequisites) {
+            nodeToMyPrereqs[edge[0]].insert(edge[1]);
+            nodeToNieghbours[edge[1]].insert(edge[0]);
+            validStart.erase(edge[0]);
+        }
+
+        // Attempt traverse
+        unordered_set<int> visited;
+        queue<int> q;
+        for (int val : validStart) {
+            q.push(val);
+        }
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            visited.insert(curr);
+
+            for (int neigh : nodeToNieghbours[curr]) {
+                nodeToMyPrereqs[neigh].erase(curr);
+
+                if (!visited.contains(neigh) && nodeToMyPrereqs[neigh].empty()) {
+                    q.push(neigh);
                 }
             }
         }
 
-
-        return true;
+        return visited.size() == numCourses;
     }
 };
