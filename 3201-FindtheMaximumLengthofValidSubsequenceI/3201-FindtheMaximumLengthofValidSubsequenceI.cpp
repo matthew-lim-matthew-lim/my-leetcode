658b3a26100b82af73892c923da3d8964d4d5eca
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-07-16 18:05:49 +1000

    File updated at 7/16/2025, 6:05:48 PM

--- 3201-FindtheMaximumLengthofValidSubsequenceI/3201-FindtheMaximumLengthofValidSubsequenceI.cpp
+++ 3201-FindtheMaximumLengthofValidSubsequenceI/3201-FindtheMaximumLengthofValidSubsequenceI.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/16/2025, 5:59:46 PM
+// Last updated: 7/16/2025, 6:05:48 PM
 /* 
 Adjacent values must be either:
 - All the same parity.
@@ -10,31 +10,36 @@ Even only -> EVEN
 Odd only -> ODD
  */
 class Solution {
-private:
-    const static int AEVEN = 0;
-    const static int AODD = 1;
-    const static int EVEN = 2;
-    const static int ODD = 3;
 
 public:
     int maximumLength(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(4, 0));
 
-        dp[0][AEVEN] = nums[0] % 2 == 0;
-        dp[0][AODD] = nums[0] % 2 == 1;
-        dp[0][EVEN] = nums[0] % 2 == 0;
-        dp[0][ODD] = nums[0] % 2 == 1;    
+        int oddFirst = 0;
+        int evenFirst = 0;
+        int numOdd = 0;
+        int numEven = 0;
 
-        for (int i = 1; i < n; i++) {
-            dp[i][AEVEN] = (nums[i] % 2 == 0) ? max(dp[i - 1][AODD] + 1, dp[i - 1][AEVEN]) : dp[i - 1][AEVEN];
-            dp[i][AODD] = (nums[i] % 2 == 1) ? max(dp[i - 1][AEVEN] + 1, dp[i - 1][AODD]) : dp[i - 1][AODD];
-            dp[i][EVEN] = dp[i - 1][EVEN] + (nums[i] % 2 == 0);
-            dp[i][ODD] = dp[i - 1][ODD] + (nums[i] % 2 == 1);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                numEven++;
+                if (evenFirst % 2 == 0) {
+                    evenFirst++;
+                } 
+                if (oddFirst % 2 == 1) {
+                    oddFirst++;
+                }
+            } else {
+                numOdd++;
+                if (oddFirst % 2 == 0) {
+                    oddFirst++;
+                } 
+                if (evenFirst % 2 == 1) {
+                    evenFirst++;
+                }
+            }
         }
-
         
-
-        return max({dp[n - 1][AEVEN], dp[n - 1][AODD], dp[n - 1][EVEN], dp[n - 1][ODD]});
+        return max({oddFirst, evenFirst, numOdd, numEven});
     }
 };
