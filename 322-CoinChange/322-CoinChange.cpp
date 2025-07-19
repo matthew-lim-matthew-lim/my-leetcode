7267851ccbbf2927073f1f27e85dd5ad9e9644fb
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-07-20 00:57:56 +1000

    File updated at 7/20/2025, 12:57:55 AM

--- 322-CoinChange/322-CoinChange.cpp
+++ 322-CoinChange/322-CoinChange.cpp
@@ -1,22 +1,24 @@
+// Last updated: 7/20/2025, 12:57:55 AM
+/* 
+Dynamic programming.
+
+dp[i] is the minimum number of coins required to make 'i'.
+
+ */
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // Dynamic Programming
-        // dp[value] = number of coins used. 
-
-        // Bottom-up
-        vector<int> dp(amount + 1, amount + 1);
+        int n = coins.size();
+        vector<int> dp(amount + 1, INT_MAX);
         dp[0] = 0;
-        for (int i = 0; i <= amount; i++) {
-            // Can we make that amount with less coins?
-            for (long long coin : coins) {
-                if (i - coin < 0) {
-                    continue;
+        for (int i = 1; i < amount + 1; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
                 }
-                dp[i] = min(dp[i - coin] + 1, dp[i]);
             }
         }
 
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
