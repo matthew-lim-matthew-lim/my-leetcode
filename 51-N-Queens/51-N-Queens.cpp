ab01e8b681e498f028a9ed37ed6102123f9c9038
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-07-22 00:39:29 +1000

    File updated at 7/22/2025, 12:39:29 AM

--- 51-N-Queens/51-N-Queens.cpp
+++ 51-N-Queens/51-N-Queens.cpp
@@ -1,97 +1,59 @@
+// Last updated: 7/22/2025, 12:39:29 AM
+/* 
+Backtracking probably.
+
+First, put Queen in pos1 and recurse, then put Queen in pos2 and recurse, etc.
+
+It's only 9, so we can mark the used squares.
+ */
+
 class Solution {
+    int n_;
+    vector<vector<int>> directions = {{1, 1}, {1, -1}, {1, 0}};
 public:
-    void diagonal_processing(vector<vector<int>>& curr_available, int n, int y, int x, int y_dir, int x_dir, int delta) {
-        int curr_y = y;
-        int curr_x = x;
-        while (curr_y < n &&
-                curr_y >= 0 && 
-                curr_x < n 
-                && curr_x >= 0
-        ) {
-            curr_available[curr_y][curr_x] += delta;
-            curr_y += y_dir;
-            curr_x += x_dir;
-        }
-    }
+    void backtrack(vector<vector<string>> &res, vector<string> currBoard, int row, vector<vector<bool>> invalidBoard) {
+        int n = n_;
 
-    void increment_hits(vector<vector<int>>& curr_available, int n, int y, int x) {
-        // Increment hits on row, col, and diagonal
-        // Row and col
-        for (int i = 0; i < n; i++) {
-            curr_available[y][i]++;
-            curr_available[i][x]++;
-        }
-
-        // Diagonals
-        diagonal_processing(curr_available, n, y, x, 1, 1, 1);
-        diagonal_processing(curr_available, n, y, x, 1, -1, 1);
-
-        // The current square was counted 6 times.
-        curr_available[y][x] -= 3;
-    }
-
-    void decrement_hits(vector<vector<int>>& curr_available, int n, int y, int x) {
-        // Increment hits on row, col, and diagonal
-        // Row and col
-        for (int i = 0; i < n; i++) {
-            curr_available[y][i]--;
-            curr_available[i][x]--;
-        }
-
-        // Diagonals
-        // Only need to process the direction of increasing 'y' since our algorithm goes row-by-row 
-        // meaning that later queens cannot be placed in the rows that have already been processed. 
-        diagonal_processing(curr_available, n, y, x, 1, 1, -1);
-        diagonal_processing(curr_available, n, y, x, 1, -1, -1);
-
-        // The current square was counted 6 times.
-        curr_available[y][x] += 3;
-    }
-
-    void backtrack(vector<vector<string>>& res, 
-                    vector<vector<int>>& curr_available, 
-                    vector<string>& curr_res, 
-                    int n, 
-                    int queen_count,
-                    int curr_y
-    ) {
-        if (queen_count == n) {
-            res.push_back(curr_res);
+        if (row >= n) {
+            res.push_back(currBoard);
             return;
         }
 
-        for (int i = curr_y; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (curr_available[i][j] > 0) {
-                    continue;
+        for (int i = 0; i < n; i++) {
+            if (!invalidBoard[row][i]) {
+                // cout << row << " " << i << endl;
+                vector<vector<bool>> newInvalid = invalidBoard;
+
+                newInvalid[row][i] = true;
+                currBoard[row][i] = 'Q';
+
+                for (vector<int> dir : directions) {
+                    int y = row;
+                    int x = i;
+                    for (int j = 0; j < n; j++) {
+                        y += dir[0];
+                        x += dir[1];
+                        if (y < n && y >= 0 && x < n && x >= 0) {
+                            newInvalid[y][x] = true;
+                        }
+                    }
                 }
-                curr_res[i][j] = 'Q';
-                increment_hits(curr_available, n, i, j);
-                backtrack(res, curr_available, curr_res, n, queen_count + 1, i);
-                decrement_hits(curr_available, n, i, j);
-                curr_res[i][j] = '.';
+
+                backtrack(res, currBoard, row + 1, newInvalid);
+
+                currBoard[row][i] = '.';
             }
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        // Took 55:00
-        // Was really tricky to think of a nice way to do it, but realise that bruteforcing with backtracking
-        // could be a straightforward way as long as there is a way to track the available squares.
-
-        // Restrictions:
-        // No 2 queens on same row. 
-        // No 2 queens on same column.
-        // No 2 queens on same diagonal. 
+        n_ = n;
         vector<vector<string>> res;
-        // The current board with just the Queens and '.'
-        vector<string> curr_res(n, string(n, '.'));
-        // The current board availabilities, where 'x' is for the squares that can't be occupied. 
-        vector<vector<int>> curr_available(n, vector<int>(n, 0));
 
-        int queen_count = 0;
+        vector<string> currBoard(n, string(n, '.'));
+        vector<vector<bool>> invalidBoard(n, vector(n, false));
 
-        backtrack(res, curr_available, curr_res, n, queen_count, 0);
+        backtrack(res, currBoard, 0, invalidBoard);
 
         return res;
     }
