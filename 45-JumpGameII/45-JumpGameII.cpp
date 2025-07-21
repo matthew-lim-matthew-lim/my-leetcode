2eff2091dcd175dbfbdd6ead54d3633944d35728
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-07-21 21:49:20 +1000

    File updated at 7/21/2025, 9:49:19 PM

--- 45-JumpGameII/45-JumpGameII.cpp
+++ 45-JumpGameII/45-JumpGameII.cpp
@@ -1,27 +1,25 @@
-// Last updated: 4/25/2025, 5:19:02 PM
+// Last updated: 7/21/2025, 9:49:19 PM
+/* 
+Dynamic Programming.
+
+Go from front to back.
+
+Each index, store the cheapest to get there. 
+ */
+
 class Solution {
 public:
-// We track reachable.
-// We keep track of the furthest reachable within the current reachable. 
-// When we reach the end of reachable, we use the furthest one.
     int jump(vector<int>& nums) {
-        int currReachable = nums[0];
-        int reachable = currReachable;
-        int currCost = 1; 
         int n = nums.size();
-        if (n == 1) {
-            return 0;
-        }
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
 
-        for (int i = 1; i < n - 1; i++) {
-            currReachable = max(currReachable, i + nums[i]);
-
-            if (i == reachable) {
-                reachable = currReachable;
-                currCost++;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n && j <= i + nums[i]; j++) {
+                dp[j] = min(dp[j], dp[i] + 1);
             }
         }
 
-        return currCost;
+        return dp[n - 1];
     }
 };
