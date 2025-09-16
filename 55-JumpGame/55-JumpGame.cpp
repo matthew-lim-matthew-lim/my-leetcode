f557b4b1a31bae0b45ebaadd1fe5becdb71270ea
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-09-16 13:57:02 +1000

    File updated at 9/16/2025, 1:57:01 PM

--- 55-JumpGame/55-JumpGame.cpp
+++ 55-JumpGame/55-JumpGame.cpp
@@ -1,19 +1,15 @@
-// Last updated: 7/21/2025, 9:21:56 PM
-/* 
-Go from back to front and instead keep track of how for we can reach.
- */
+// Last updated: 9/16/2025, 1:57:01 PM
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        
-        int end = n - 1;
+        int currEnd = n - 1;
         for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] + i >= end) {
-                end = i;
+            if (i + nums[i] >= currEnd) {
+                currEnd = i;
             }
         }
 
-        return end == 0;
+        return currEnd == 0;
     }
 };
