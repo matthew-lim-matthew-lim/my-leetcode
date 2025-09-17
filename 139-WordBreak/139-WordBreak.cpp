59e0874d7c3a69c4acac0b50b1b435304055abbb
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-09-17 23:35:30 +1000

    File updated at 9/17/2025, 11:35:30 PM

--- 139-WordBreak/139-WordBreak.cpp
+++ 139-WordBreak/139-WordBreak.cpp
@@ -1,30 +1,49 @@
+// Last updated: 9/17/2025, 11:35:30 PM
+/* 
+dp[i] represents if it is possible to use wordDict to construct to 'i'.
+ */
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict{wordDict.begin(), wordDict.end()};
+        int n = s.size();
+        vector<bool> dp(n, false);
 
-        // Iterate through the string s, adding letters one by one. 
-        // Let k be the current string.
-        // Let w be a particular word in wordDict. Loop through all the words in wordDict.
-        // dp(k) = dp(k - w)
-        // Would be like unordered_set.contains(k - w).
-        
-        string k;
-        for (int i = 0; i < s.size(); i++) {
-            k.push_back(s[i]);
-            for (string& w : wordDict) {
-                // Check if k ends with w
-                if (k.size() >= w.size() && k.compare(k.size() - w.size(), w.size(), w) == 0) {
-                    // Compute k - w
-                    string kMinusW = k.substr(0, k.size() - w.size());
-                    // Check if k - w is in the set
-                    if (dict.contains(kMinusW)) {
-                        dict.insert(k);
+        for (int i = 0; i < n; i++) {
+            for (string &word : wordDict) {
+                cout << word << endl;
+                cout << "1 " << endl;
+                bool valid = true;
+                cout << int(i - word.size() + 1) << endl;
+                if (int(i - word.size() + 1) < 0) {
+                    cout << "hey" << endl;
+                    continue;
+                }
+
+                cout << "2 " << endl;
+                for (int j = 0; j < word.size(); j++) {
+                    cout << "a" << endl;
+                    if (s[i - word.size() + 1 + j] != word[j]) {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                cout << "3 " << endl;
+                cout << word << " " << valid << endl;
+                if (valid) {
+                    if (i - word.size() == -1) {
+                        dp[i] = true;
+                    } else {
+                        dp[i] = dp[i] | dp[i - word.size()];
                     }
                 }
             }
         }
 
-        return dict.contains(s);
+        for (int i = 0; i < n; i++) {
+            cout << dp[i] << " ";
+        }
+
+        return dp[n - 1];
     }
 };
