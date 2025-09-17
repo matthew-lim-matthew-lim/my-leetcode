d28f438a45b42bf3aeee5fb130faebe9491c87e3
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-09-18 00:18:38 +1000

    File updated at 9/18/2025, 12:18:37 AM

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,19 +1,16 @@
-// Last updated: 4/5/2025, 4:05:00 PM
+// Last updated: 9/18/2025, 12:18:37 AM
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // Each night, can rob any house before but not the one right before.
         int n = nums.size();
-        if (n == 1) {
-            return nums[0];
-        }
         vector<int> dp(n, 0);
+
         dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        if (n > 1) dp[1] = max(nums[0], nums[1]);
         for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+            dp[i] = max(max(dp[i], dp[i - 1]), dp[i - 2] + nums[i]);
         }
 
-        return dp[n - 1];
+        return max(dp[n - 1], n > 1 ? dp[n - 2] : 0);
     }
 };
