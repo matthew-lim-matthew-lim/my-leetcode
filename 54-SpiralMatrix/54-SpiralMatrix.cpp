a32482a4b4a280aa3ce14e9449221629f63497c4
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-09-18 01:21:51 +1000

    File updated at 9/18/2025, 1:21:51 AM

--- 54-SpiralMatrix/54-SpiralMatrix.cpp
+++ 54-SpiralMatrix/54-SpiralMatrix.cpp
@@ -1,41 +1,27 @@
-// Last updated: 7/9/2025, 9:44:29 PM
+// Last updated: 9/18/2025, 1:21:51 AM
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> pos = {0, 0};
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        int R = m - 1;
-        int D = n - 1;
-        int L = 0;
-        int U = 0;
+        int u = 0;
+        int d = matrix.size() - 1;
+        int l = 0;
+        int r = matrix[0].size() - 1;
 
         vector<int> res;
-
-        while (L <= R && U <= D) {
-            for (int j = L; j <= R; j++) {
-                res.push_back(matrix[U][j]);
+        while (u <= d && l <= r) {
+            for (int x = l; x <= r; x++) res.push_back(matrix[u][x]);
+            u++;
+            for (int y = u; y <= d; y++) res.push_back(matrix[y][r]);
+            r--;
+            if (u <= d) {
+                for (int x = r; x >= l; x--) res.push_back(matrix[d][x]);
+                d--;
             }
-            U++;
-            for (int i = U; i <= D; i++) {
-                res.push_back(matrix[i][R]);
-            }
-            R--;
-            if (U <= D) {
-                for (int j = R; j >= L; j--) {
-                    res.push_back(matrix[D][j]);
-                }
-                D--;
-            }
-            if (L <= R) {
-                for (int i = D; i >= U; i--) {
-                    res.push_back(matrix[i][L]);
-                }
-                L++;
+            if (l <= r) {
+                for (int y = d; y >= u; y--) res.push_back(matrix[y][l]);
+                l++;
             }
         }
-
         return res;
     }
 };
