17db7588e71fef50da826501ef7bbb1e48312e33
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-09-24 21:12:56 +1000

    File updated at 9/24/2025, 9:12:56 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
@@ -1,49 +1,40 @@
-// Last updated: 9/24/2025, 9:07:38 PM
-/* 
-Whenever we have a mismatch, we can eliminate a rectangle of numbers. 
-Eg. if matrix[i][j] < target, then eliminate matrix[0..i][0..j].
-If matrix[i][j] > target, then eliminate matrix[i..n-1][j..m-1].
-
-This is now a recursion problem with 3 rectangular search areas.
- */
+// Last updated: 9/24/2025, 9:12:56 PM
 class Solution {
-private:
-    int target_;
-    vector<vector<int>> matrix_;
-    int n_;
-    int m_;
 public:
-    bool search(int tly, int tlx, int bry, int brx) {
-        if (tly > bry || tlx > brx || 
-            bry >= n_ || bry < 0 || tly >= n_ || tly < 0 ||
-            tlx >= m_ || tlx < 0 || brx >= m_ || brx < 0
-        ) {
-            return false;
-        }
-        int midy = (tly + bry) / 2;
-        int midx = (tlx + brx) / 2;
-
-        if (matrix_[midy][midx] > target_) {
-            // Eliminate bottom-right
-            return search(tly, tlx, midy - 1, midx - 1) ||
-                search(tly, midx, midy - 1, brx) ||
-                search(midy, tlx, bry, midx - 1);
-        } else if (matrix_[midy][midx] < target_) {
-            // Eliminate top-left
-            return search(midy + 1, midx + 1, bry, brx) ||
-                search(tly, midx + 1, midy, brx) ||
-                search(midy + 1, tlx, bry, midx);
-        }
-        return true;
-    }
-
+    // int bs(vector<int>& arr, int x){
+    //     int low = 0; int high = arr.size();
+    //     while(low <= high){
+    //         int mid = (low+high)/2;
+    //         if (arr[mid] == x) return 1;
+    //         else if (arr[mid] > x) high = mid -1;
+    //         else low = mid + 1;
+    //     }
+    //     return 0;
+    // }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        n_ = matrix.size();
-        m_ = matrix[0].size();
+        // int rows = matrix.size();
+        // int cols = matrix[0].size();
+        // int ans = -1;
+        // for (int i = 0; i < rows; i++) {
+        //     if (matrix[i][0] <= target && target <= matrix[i][cols-1]) {
+        //         ans = bs(matrix[i], target);
+        //     }else continue;
+        // }
 
-        target_ = target;
-        matrix_ = matrix;
+        // return ans == 1 ? true: false;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low = 0; int high = (n*m) - 1;
+        while (low <= high) {
+            int mid = (low+high)/2;
+            int midrow = mid / m;
+            int midcol = mid % m;
+            int midEl = matrix[midrow][midcol];
+            if (midEl == target) return true;
+            else if (midEl > target) high = mid - 1;
+            else low = mid + 1;
+        }
 
-        return search(0, 0, n_ - 1, m_ - 1);
+        return false;
     }
 };
