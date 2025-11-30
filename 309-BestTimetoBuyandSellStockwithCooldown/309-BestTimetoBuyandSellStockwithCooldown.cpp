47a422cb6dcb64912d87b08e179e9709e22045d0
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-11-30 19:52:32 +1100

    File updated at 11/30/2025, 7:52:31 PM

--- 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.cpp
+++ 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.cpp
@@ -1,35 +1,36 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        // State machine DP.
-        // States are buy (0), sell (1), cooldown (2).
-        // dp[i][0] means the best profit if you bought at `i`.
-        // dp[i][1] means the best profit if you sold at `i`.
-        // dp[i][2] means the best profit if you cooldown at `i`.
-        // buy -> sell -> cooldown 
-        // dp[i][0] = max{k < i}(dp[k][2]); models cooldown -> buy.
-        // dp[i][1] = max{k < i}(dp[k][0] + prices[i] - prices[k]); models buy -> sell.
-        // dp[i][2] = max{k < i}(dp[k][1]); models sell -> cooldown.
-
-        // Process in increasing order of `i`, and for each `i`, process in increasing order of `0` to `2`.
-        // This is because we always need each of `0` to `2` for every level of `i`. 
-        vector<vector<int>> dp(prices.size(), vector<int>(3, 0));
-
-        for (int i = 1; i < prices.size(); i++) {
-            // dp[i][0] = max{k < i}(dp[k][2]); models cooldown -> buy.
-            for (int k = 0; k < i; k++) {
-                dp[i][0] = max(dp[i][2], dp[k][2]);
-            }
-            // dp[i][1] = max{k < i}(dp[k][0] + prices[i] - prices[k]); models buy -> sell.
-            for (int k = 0; k < i; k++) {
-                dp[i][1] = max(dp[i][1], dp[k][0] + prices[i] - prices[k]);
-            }
-            // dp[i][2] = max{k < i}(dp[k][1]); models sell -> cooldown.
-            for (int k = 0; k < i; k++) {
-                dp[i][2] = max(dp[i][2], dp[k][1]);
-            }
-        }
-
-        return max({dp[prices.size() - 1][0], dp[prices.size() - 1][1], dp[prices.size() - 1][2]});
-    }
-};
+// Last updated: 11/30/2025, 7:52:31 PM
+1/* 
+2Dynamic programming with state machine?
+3
+4dp[index][state=buy/sell/cooldown/hold]
+5
+6buy <- cooldown
+7sell <- buy/hold
+8cooldown <- sell/cooldown
+9hold <- buy/hold
+10
+11At each one, do the max of the possible previous states
+12 */
+13
+14class Solution {
+15public:
+16    int maxProfit(vector<int>& prices) {
+17        int n = prices.size();
+18
+19        vector<vector<int>> dp(n, vector<int>(4, 0));
+20
+21        dp[0][0] = -prices[0];
+22        dp[0][1] = 0;
+23        dp[0][2] = 0;
+24        dp[0][3] = -prices[0];
+25
+26        for (int i = 1; i < n; i++) {
+27            dp[i][0] = dp[i - 1][2] - prices[i];
+28            dp[i][1] = max(dp[i - 1][0] + prices[i], dp[i - 1][3] + prices[i]);
+29            dp[i][2] = max(dp[i - 1][1], dp[i - 1][2]);
+30            dp[i][3] = max(dp[i - 1][0], dp[i - 1][3]);
+31        }
+32
+33        return max({ dp[n - 1][1], dp[n - 1][2], dp[n - 1][3]});
+34    }
+35};
