d152b35fbff5604308c84ecca566d2ab0ced1382
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-11-30 23:40:21 +1100

    File updated at 11/30/2025, 11:40:20 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -1,30 +1,30 @@
-// Last updated: 4/10/2025, 4:58:09 PM
-class Solution {
-public:
-    int longestCommonSubsequence(string text1, string text2) {
-        // If last char equal, +1 to current longest.
-        // If last char not equal, repeat for text1 with 1 less char and text2, as well as text1 and text2 with 1 less char.
-        // if (text1.back() == text2.back()), 
-        // dp[text1][text2] = dp[text1 - 1][text2 - 1] + 1;
-        // else 
-        // dp[text1][text2] = max(dp[text1 - 1][text2], dp[text1][text2 - 1]);
-        int n = text1.size();
-        int m = text2.size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int TL = (i - 1 >= 0 && j - 1 >= 0) ? dp[i - 1][j - 1] : 0;
-                int TT = (i - 1 >= 0) ? dp[i - 1][j] : 0;
-                int LL = (j - 1 >= 0) ? dp[i][j - 1] : 0;
-
-                if (text1[i] == text2[j]) {
-                    dp[i][j] = TL + 1;
-                } else {
-                    dp[i][j] = max(TT, LL);
-                }
-            }
-        }
-
-        return dp[n - 1][m - 1];
-    }
-};
+// Last updated: 11/30/2025, 11:40:20 PM
+1/* 
+2dp[i][j] = length
+3dp[i index in text1][j index in text2] = length of combined subsequence
+4
+5- text1[i] == text2[j]: dp[i - 1][j - 1] + 1
+6- text1[i] != text2[j]: max(dp[i - 1][j], dp[i][j - 1]);
+7 */
+8class Solution {
+9public:
+10    int longestCommonSubsequence(string text1, string text2) {
+11        int n = text1.size() + 1;
+12        int m = text2.size() + 1;
+13
+14        vector<vector<int>> dp(n, vector<int>(m, 0));
+15
+16        for (int i = 1; i < n; i++) {
+17            for (int j = 1; j < m; j++) {
+18                if (text1[i-1] == text2[j-1]) {
+19                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1);
+20                } else {
+21                    dp[i][j] = max(dp[i][j], dp[i-1][j]);
+22                    dp[i][j] = max(dp[i][j], dp[i][j-1]);
+23                }
+24            }
+25        }
+26
+27        return dp[n-1][m-1];
+28    }
+29};
