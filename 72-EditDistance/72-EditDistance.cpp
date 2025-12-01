6516aa1aaf4acc9409322fc808be76ef6539cd08
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-12-01 20:06:05 +1100

    File updated at 12/1/2025, 8:06:04 PM

--- 72-EditDistance/72-EditDistance.cpp
+++ 72-EditDistance/72-EditDistance.cpp
@@ -1,53 +1,58 @@
-// Last updated: 9/24/2025, 10:28:22 PM
-#include <string>
-#include <vector>
-#include <algorithm> // For std::min
-
-class Solution {
-public:
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-
-        // dp[i][j] will store the minimum edit distance between
-        // word1's first 'i' characters and word2's first 'j' characters.
-        // We use (n+1) x (m+1) dimensions to handle empty string prefixes.
-        std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1));
-
-        // Base cases:
-        // If word1 is empty (i=0), we need 'j' insertions to form word2's first 'j' chars.
-        for (int j = 0; j <= m; ++j) {
-            dp[0][j] = j;
-        }
-        // If word2 is empty (j=0), we need 'i' deletions to transform word1's first 'i' chars
-        // into an empty string.
-        for (int i = 0; i <= n; ++i) {
-            dp[i][0] = i;
-        }
-
-        // Fill the DP table
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                // If the current characters match, no operation is needed for them.
-                // The distance is simply the distance of the previous prefixes.
-                if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    // If characters do not match, we consider three operations:
-                    // 1. Replace: Replace word1[i-1] with word2[j-1]. Cost: dp[i-1][j-1] + 1
-                    // 2. Delete: Delete word1[i-1]. Cost: dp[i-1][j] + 1
-                    // 3. Insert: Insert word2[j-1] into word1. Cost: dp[i][j-1] + 1
-                    // We choose the minimum cost among these options.
-                    dp[i][j] = std::min({
-                        dp[i - 1][j - 1] + 1, // Replace
-                        dp[i - 1][j] + 1,     // Delete
-                        dp[i][j - 1] + 1      // Insert
-                    });
-                }
-            }
-        }
-
-        // The result is the edit distance between the full word1 and full word2.
-        return dp[n][m];
-    }
-};
+// Last updated: 12/1/2025, 8:06:04 PM
+1/* 
+2- For each letter in word1, you can insert/delete/replace. 
+3
+4- Consider if we changed that letter.
+5- dp[i][j]
+6
+7- dp[i index word1][j index word2] = min number of edits to get here. 
+8
+9dp[0][0] = 1 -> Turn h into r
+10dp[0][1] = 2 -> Turn h into r, add o
+11dp[1][1] = 1 -> Turn h into r
+12dp[1][2] = 2 -> Turn h into r, add s
+13dp[2][2] = 2 -> Turn h into r, Turn r into s
+14dp[3][2] =  -> Turn 
+15
+16dp[i][j]
+17
+18case 1: word[i] == word[j]
+19dp[i][j] = dp[i-1][j-1];
+20
+21case 2: word[i] != word[j]
+22dp[i][j] = 
+23min(
+24    dp[i-1][j-1] + 1,
+25    dp[i-1][j] + 1,
+26    dp[i][j-1] + 1
+27)
+28 */
+29class Solution {
+30public:
+31    int minDistance(string word1, string word2) {
+32        int n = word1.size();
+33        int m = word2.size();
+34
+35        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));
+36        for (int i = 0; i < n + 1; i++) {
+37            dp[i][0] = i;
+38        } 
+39        for (int j = 0; j < m + 1; j++) {
+40            dp[0][j] = j;
+41        }
+42
+43        for (int i = 1; i < n + 1; i++) {
+44            for (int j = 1; j < m + 1; j++) {
+45                if (word1[i-1] == word2[j-1]) {
+46                    dp[i][j] = dp[i-1][j-1];
+47                } else {
+48                    dp[i][j] = min(dp[i][j], dp[i-1][j-1] + 1);
+49                    dp[i][j] = min(dp[i][j], dp[i-1][j] + 1);
+50                    dp[i][j] = min(dp[i][j], dp[i][j-1] + 1);
+51                }
+52            }
+53        }
+54
+55        return dp[n][m];
+56    }
+57};
