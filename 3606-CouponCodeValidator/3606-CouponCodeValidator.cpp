99396f3113b4196110dd7ddc7673b1d27cd80f8f
Matthew Lim <matthewlimcenterdiv@gmail.com>
2025-12-14 00:44:18 +1100

    File updated at 12/14/2025, 12:44:18 AM

--- 3606-CouponCodeValidator/3606-CouponCodeValidator.cpp
+++ 3606-CouponCodeValidator/3606-CouponCodeValidator.cpp
@@ -1,38 +1,67 @@
-// Last updated: 12/13/2025, 11:57:48 PM
-1class Solution {
-2public:
-3    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
-4        // Process them and put all the valid ones into a vector of pairs, sort that, and then return result.
-5        int n = code.size();
-6
-7        vector<string> res;
-8
-9        vector<pair<string, string>> pairs;
-10
-11        regex r("^[a-zA-Z0-9_]*$");
-12        
-13        for (int i = 0; i < n; i++) {
-14            if (!isActive[i] || !regex_match(code[i], r) || code[i].size() == 0) {
-15                continue;
-16            }
-17
-18            if (!(businessLine[i] == "electronics" || 
-19                 businessLine[i] == "grocery" ||
-20                 businessLine[i] == "pharmacy" ||
-21                 businessLine[i] == "restaurant"
-22            )) {
-23                continue;
-24            } 
-25
-26            pairs.push_back({ businessLine[i], code[i] });
-27        }
-28
-29        sort(pairs.begin(), pairs.end());
-30
-31        for (pair<string, string> &pair : pairs) {
-32            res.push_back(pair.second);
-33        }
-34
-35        return res;
-36    }
-37};
+// Last updated: 12/14/2025, 12:44:18 AM
+1/* 
+2Put all the valid codes of each category in a vector for each. 
+3Then sort the codes.
+4Then concatenate the vectors.
+5 */
+6class Solution {
+7public:
+8    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+9        vector<string> elecCodes;
+10        vector<string> grocCodes;
+11        vector<string> pharCodes;
+12        vector<string> restCodes;
+13
+14        int n = code.size();
+15
+16        for (int i = 0; i < n; i++) {
+17            bool valid = true;
+18            if (code[i].empty()) {
+19                valid = false;
+20            }
+21            for (char c : code[i]) {
+22                if ((c <= 'z' && c >= 'a') ||
+23                    (c <= 'Z' && c >= 'A') ||
+24                    (c <= '9' && c >= '0') ||
+25                    (c == '_')
+26                ) {
+27                    continue;
+28                }
+29                valid = false;
+30                break;
+31            }
+32
+33            if (!isActive[i]) {
+34                valid = false;
+35            }
+36
+37            if (!valid) {
+38                continue;
+39            }
+40
+41            if (businessLine[i] == "electronics") {
+42                elecCodes.push_back(code[i]);
+43            }
+44            if (businessLine[i] == "grocery") {
+45                grocCodes.push_back(code[i]);
+46            }
+47            if (businessLine[i] == "pharmacy") {
+48                pharCodes.push_back(code[i]);
+49            }
+50            if (businessLine[i] == "restaurant") {
+51                restCodes.push_back(code[i]);
+52            }
+53        }
+54
+55        sort(elecCodes.begin(), elecCodes.end());
+56        sort(grocCodes.begin(), grocCodes.end());
+57        sort(pharCodes.begin(), pharCodes.end());
+58        sort(restCodes.begin(), restCodes.end());
+59
+60        elecCodes.insert(elecCodes.end(), grocCodes.begin(), grocCodes.end());
+61        elecCodes.insert(elecCodes.end(), pharCodes.begin(), pharCodes.end());
+62        elecCodes.insert(elecCodes.end(), restCodes.begin(), restCodes.end());
+63
+64        return elecCodes;
+65    }
+66};
