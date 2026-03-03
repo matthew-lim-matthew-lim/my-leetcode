96418b1b289157fecd60a0027b4835c292ac0998
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-03-04 01:34:32 +1100

    File updated at 3/4/2026, 1:34:32 AM

--- 146-LRUCache/146-LRUCache.cs
+++ 146-LRUCache/146-LRUCache.cs
@@ -1,56 +1,48 @@
-// Last updated: 3/4/2026, 1:09:42 AM
-1public class LRUCache {
-2    public class Entry {
-3        public int key;
-4        public int val;
-5        public Entry(int k, int v) { key=k; val=v; }
-6    }
-7
-8    public LinkedList<Entry> ll_;
-9    public Dictionary<int, LinkedListNode<Entry>> dict_ = new Dictionary<int, LinkedListNode<Entry>>();
-10    public int capacity_;
-11
-12    public LRUCache(int capacity) {
-13        ll_ = new LinkedList<Entry>();
-14        capacity_ = capacity;
-15    }
-16    
-17    public int Get(int key) {
-18        if (!dict_.ContainsKey(key)) {
-19            return -1;
-20        }
-21
-22        int value = dict_[key].Value.val;
-23        ll_.Remove(dict_[key]);
-24        dict_.Remove(key);
-25
-26        ll_.AddFirst(new Entry(key, value));
-27        dict_[key] = ll_.First;
+// Last updated: 3/4/2026, 1:34:32 AM
+1/* 
+2Just do it with events.
+3 */
+4public class Solution {
+5    public class Event {
+6        public int timestamp;
+7        public bool end;
+8        public Event(int t, bool e) { timestamp = t; end = e; }
+9    }
+10    
+11    public int[][] Merge(int[][] intervals) {
+12        List<Event> events = new List<Event>();
+13        foreach (int[] interval in intervals) {
+14            events.Add(new Event(interval[0], false));
+15            events.Add(new Event(interval[1], true));
+16        }
+17        events.Sort((a, b) => {
+18            int result = a.timestamp.CompareTo(b.timestamp);
+19
+20            if (result == 0) {
+21                return a.end.CompareTo(b.end);
+22            }
+23
+24            return result;
+25        });
+26
+27        List<int[]> merged = new List<int[]>();
 28
-29        return value;
-30    }
-31    
-32    public void Put(int key, int value) {
-33        if (dict_.ContainsKey(key)) {
-34            ll_.Remove(dict_[key]);
-35            dict_.Remove(key);
-36        } else if (!dict_.ContainsKey(key)) {
-37            if (capacity_ == 0) {
-38                dict_.Remove(ll_.Last.Value.key);
-39                ll_.RemoveLast();
-40            } else {
-41                capacity_--;
+29        int currOpen = 0;
+30        int prevOpen = 0;
+31        foreach (Event e in events) {
+32            if (currOpen == 0) {
+33                prevOpen = e.timestamp;
+34            }
+35            if (!e.end) {
+36                currOpen++;
+37            } else {
+38                currOpen--;
+39            }
+40            if (currOpen == 0) {
+41                merged.Add(new int[] { prevOpen, e.timestamp });
 42            }
 43        }
-44        
-45        ll_.AddFirst(new Entry(key, value));
-46        dict_[key] = ll_.First;
-47    }
-48}
-49
-50/**
-51 * Your LRUCache object will be instantiated and called as such:
-52 * LRUCache obj = new LRUCache(capacity);
-53 * int param_1 = obj.Get(key);
-54 * obj.Put(key,value);
-55 */
+44
+45        return merged.ToArray();
+46    }
+47}
