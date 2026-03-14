d3aa4a276cdb0036b14d9374a4f44179bd0995a8
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-03-14 14:59:14 +1100

    File updated at 3/14/2026, 2:59:13 PM

--- 198-HouseRobber/198-HouseRobber.py
+++ 198-HouseRobber/198-HouseRobber.py
@@ -1,4 +1,4 @@
-# Last updated: 3/14/2026, 2:57:09 PM
+# Last updated: 3/14/2026, 2:59:13 PM
 1"""
 2dp[i] = max[dp[i-1], dp[i-2] + nums[i])
 3"""
@@ -11,12 +11,12 @@
 10        elif n == 2:
 11            return max(nums[0], nums[1]) 
 12
-13        dp: list[int] = [ 0 for i in range(n) ]
-14
-15        dp[0] = nums[0]
-16        dp[1] = max(nums[0], nums[1])
-17
-18        for i in range(2, n):
-19            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+13        prev: int = nums[0]
+14        curr: int = max(nums[0], nums[1])
+15
+16        for i in range(2, n):
+17            new_curr = max(curr, prev + nums[i])
+18            prev = curr
+19            curr = new_curr
 20
-21        return dp[n-1]
+21        return new_curr
