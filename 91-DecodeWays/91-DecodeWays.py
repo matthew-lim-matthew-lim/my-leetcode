b3c9397c70816bf5500e6409f553bf40859c49b7
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-03-14 19:40:17 +1100

    File updated at 3/14/2026, 7:40:16 PM

--- 91-DecodeWays/91-DecodeWays.py
+++ 91-DecodeWays/91-DecodeWays.py
@@ -1,4 +1,4 @@
-# Last updated: 3/14/2026, 7:39:15 PM
+# Last updated: 3/14/2026, 7:40:16 PM
 1""" 
 2At each index, look at the previous 2 values and see if you can make a character using them.
 3
@@ -29,27 +29,32 @@
 28        dp: list[int] = [0 for i in range(n)]
 29
 30        dp[0] = int(s[0] <= "9" and s[0] >= "1") 
-31        if n == 1:
-32            return dp[0]
-33
-34        dp[1] = (dp[0] and s[1] <= "9" and s[1] >= "1") + int(s[0] != "0" and s[0:2] <= "26")
-35        if n == 2:
-36            return dp[1]
-37
-38        for i in range(2, n):
-39            curr_digit = s[i] <= "9" and s[i] >= "1"
-40            prev_digit = s[i-1] != "0" and s[i-1:i+1] <= "26"
-41            print(curr_digit, prev_digit)
+31
+32        if n == 1:
+33            return dp[0]
+34
+35        dp[1] = (
+36            (dp[0] and s[1] <= "9" and s[1] >= "1") + 
+37            int(s[0] != "0" and s[0:2] <= "26")
+38        )
+39
+40        if n == 2:
+41            return dp[1]
 42
-43            if not curr_digit and not prev_digit:
-44                return 0
-45
-46            if curr_digit:
-47                dp[i] += dp[i-1]
-48            if prev_digit:
-49                dp[i] += dp[i-2]
+43        for i in range(2, n):
+44            curr_digit = s[i] <= "9" and s[i] >= "1"
+45            prev_digit = s[i-1] != "0" and s[i-1:i+1] <= "26"
+46            print(curr_digit, prev_digit)
+47
+48            if not curr_digit and not prev_digit:
+49                return 0
 50
-51        # print(dp)
-52
-53        return dp[n-1]
-54
+51            if curr_digit:
+52                dp[i] += dp[i-1]
+53            if prev_digit:
+54                dp[i] += dp[i-2]
+55
+56        # print(dp)
+57
+58        return dp[n-1]
+59
