3ddee326c54530cb3bb6f9998b1c8e110f174981
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-03-29 19:59:31 +1100

    File updated at 3/29/2026, 7:59:30 PM

--- 3499-MaximizeActiveSectionwithTradeI/3499-MaximizeActiveSectionwithTradeI.cpp
+++ 3499-MaximizeActiveSectionwithTradeI/3499-MaximizeActiveSectionwithTradeI.cpp
@@ -1,4 +1,4 @@
-// Last updated: 3/29/2026, 7:59:10 PM
+// Last updated: 3/29/2026, 7:59:30 PM
 1/* 
 2Can convert a window of '0' with a '1' into a complete section of '1'. 
 3
@@ -35,50 +35,46 @@
 34        int l = 0;
 35        for (; r < n; r++) {
 36            if (s[r] == '1') {
-37                // cout << r << endl;
-38                currCount++;
-39            }
-40            if (r > 0 && s[r] == '0' && s[r-1] == '1') {
-41                blockCount++;
-42            }
-43            while (blockCount > 2) {
-44                if (l < n && s[l] == '1') {
-45                    currCount--;
-46                }
-47                l++;
-48                if (l < n && s[l] == '0' && s[l-1] == '1') {
-49                    blockCount--;
-50                }
-51                if (l >= n) {
-52                    break;
-53                }
-54            }
-55            if (blockCount == 1) {
-56                // cout << "YOYO" << endl;
-57                int next0 = l;
-58                int thisCurr = currCount;
-59                bool breakAction = false;
-60                while (next0 < n && s[next0] == '1') {
-61                    thisCurr--;
-62                    next0++;
-63                    if (s[next0] == '0' && s[next0-1] == '1') {
-64                        breakAction = true;
-65                    }
-66                }
-67                if (breakAction) {
-68                    continue;
-69                }
-70                // cout << "GRRR " << next0 << " " << r << endl;
-71                res = max(res, r - next0 + 1 + baseLine - thisCurr);
-72            } else if (blockCount == 2) {
-73                while (s[l] == '0') {
-74                    l++;
-75                }
-76                // cout << r << " " << l << " " << currCount << " " << baseLine << endl;
-77                res = max(res, r - l + 1 + baseLine - currCount);
-78            }
-79        }
-80
-81        return res;
-82    }
-83};
+37                currCount++;
+38            }
+39            if (r > 0 && s[r] == '0' && s[r-1] == '1') {
+40                blockCount++;
+41            }
+42            while (blockCount > 2) {
+43                if (l < n && s[l] == '1') {
+44                    currCount--;
+45                }
+46                l++;
+47                if (l < n && s[l] == '0' && s[l-1] == '1') {
+48                    blockCount--;
+49                }
+50                if (l >= n) {
+51                    break;
+52                }
+53            }
+54            if (blockCount == 1) {
+55                int next0 = l;
+56                int thisCurr = currCount;
+57                bool breakAction = false;
+58                while (next0 < n && s[next0] == '1') {
+59                    thisCurr--;
+60                    next0++;
+61                    if (s[next0] == '0' && s[next0-1] == '1') {
+62                        breakAction = true;
+63                    }
+64                }
+65                if (breakAction) {
+66                    continue;
+67                }
+68                res = max(res, r - next0 + 1 + baseLine - thisCurr);
+69            } else if (blockCount == 2) {
+70                while (s[l] == '0') {
+71                    l++;
+72                }
+73                res = max(res, r - l + 1 + baseLine - currCount);
+74            }
+75        }
+76
+77        return res;
+78    }
+79};
