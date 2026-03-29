454ecb931e68b35b87cba4177eebd352af61b2d6
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-03-29 15:05:03 +1100

    File updated at 3/29/2026, 3:05:03 PM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -1,24 +1,47 @@
-// Last updated: 3/29/2026, 2:52:48 PM
-1/* 
-2Just use a multiset?
-3*/
-4class Solution {
-5public:
-6    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-7        int n = nums.size();
-8        multiset<int> window;
-9        for (int i = 0; i < k; i++) {
-10            window.insert(nums[i]);
-11        }
-12
-13        vector<int> res = { *prev(window.end()) };
-14
-15        for (int i = k; i < n; i++) {
-16            window.erase(window.find(nums[i-k]));
-17            window.insert(nums[i]);
-18            res.push_back(*prev(window.end()));
-19        } 
-20
-21        return res;
-22    }
-23};
+// Last updated: 3/29/2026, 3:05:03 PM
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> result;
+        
+        if (n == 0 || k == 0) {
+            return result;
+        }
+        
+        if (k == 1) {
+            return nums;
+        }
+        
+        int num_windows = n - k + 1;
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+        
+        left[0] = nums[0];
+        right[n - 1] = nums[n - 1];
+        
+        for (int i = 1; i < n; ++i) {
+            if (i % k == 0) {
+                left[i] = nums[i];
+            } else {
+                left[i] = max(nums[i], left[i - 1]);
+            }
+            
+            int endIdx = n - i - 1;
+            
+            if ((endIdx + 1) % k == 0) {
+                right[endIdx] = nums[endIdx];
+            } else {
+                right[endIdx] = max(right[endIdx + 1], nums[endIdx]);
+            }
+        }
+        
+        result.resize(num_windows); // num of windows
+        
+        for (int i = 0; i < num_windows; ++i) {
+            result[i] = max(left[i + k - 1], right[i]);
+        }
+        
+        return result;
+    }
+};
