7657ece8100cb4f2874a5dc7010ddf92c11c446b
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-03-29 16:19:35 +1100

    File updated at 3/29/2026, 4:19:35 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
@@ -1,57 +1,44 @@
-class Solution {
-public:
-    bool valid(unordered_map<char, int>& char_map) {
-        for (pair<char, int> p : char_map) {
-            if (p.second > 0) {
-                cout << "false" << endl;
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    string minWindow(string s, string t) {
-        // Use a hash table to keep track of counts. 
-        // Move/iterate the right pointer to include stuff in the hash table. 
-        // While the hash table satisfies 't', move the left pointer to exclude stuff. 
-        int smallest_left = 0;
-        int smallest_right = s.size();
-        unordered_map<char, int> char_map;
-        for (char c : t) {
-            char_map[c]++;
-        }
-
-        int l_ptr = 0;
-        for (int r_ptr = 0; r_ptr < s.size(); r_ptr++) {
-            if (char_map.find(s[r_ptr]) != char_map.end()) {
-                char_map[s[r_ptr]]--;
-            }
-
-            while (valid(char_map) && l_ptr < r_ptr) {
-                if (r_ptr - l_ptr < smallest_right - smallest_left) {
-                    smallest_right = r_ptr;
-                    smallest_left = l_ptr;
-                }
-
-                if (char_map.find(s[l_ptr]) != char_map.end()) {
-                    char_map[s[l_ptr]]++;
-                }
-                l_ptr++;
-            }
-            if (valid(char_map)) {
-                if (r_ptr - l_ptr < smallest_right - smallest_left) {
-                    cout << r_ptr << " " << l_ptr << endl;
-                    smallest_right = r_ptr;
-                    smallest_left = l_ptr;
-                } 
-            }
-        }
-
-        if (smallest_right != s.size()) {
-            return s.substr(smallest_left, smallest_right - smallest_left + 1);
-        } else {
-            return "";
-        }
-    }
-};
+// Last updated: 3/29/2026, 4:19:35 PM
+1/* 
+2Keep an unordered_map with `t` and update the counts.
+3Check each step that the count of each character of the unordered_map is beyond `t`. 
+4*/
+5class Solution {
+6private:
+7    unordered_map<char, int> cMap;
+8    unordered_map<char, int> target;
+9public:
+10    bool checkCounts() {
+11        for (const pair<char, int> &p : target) {
+12            if (!cMap.contains(p.first) || cMap[p.first] < p.second) {
+13                return false;
+14            }
+15        } 
+16
+17        return true;
+18    }
+19
+20    string minWindow(string s, string t) {
+21        for (char c : t) {
+22            target[c]++;
+23        }
+24
+25        int n = s.size();
+26
+27        pair<int, int> best = {0, INT_MAX};
+28        
+29        int l_ptr = 0;
+30        for (int r_ptr = 0; r_ptr < n; r_ptr++) {
+31            cMap[s[r_ptr]]++;
+32            while (checkCounts()) {
+33                if (r_ptr - l_ptr < best.second - best.first) {
+34                    best = { l_ptr, r_ptr };
+35                }
+36                cMap[s[l_ptr]]--;
+37                l_ptr++;
+38            }
+39        }
+40
+41        return best.second == INT_MAX ? "" : s.substr(best.first, best.second - best.first + 1);
+42    }
+43};
