066996fe129e598209a72606ef997d4a3025dfad
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-03-29 14:52:48 +1100

    File updated at 3/29/2026, 2:52:48 PM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -1,27 +1,24 @@
-class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // Using a monotonic queue
-        vector<int> res;
-        // val : index
-        deque<pair<int, int>> mono_q;
-
-        int l_ptr = 0;
-
-        for (int r_ptr = 0; r_ptr < nums.size(); r_ptr++) {
-            while (!mono_q.empty() && mono_q.back().first < nums[r_ptr]) {
-                mono_q.pop_back();
-            }
-            mono_q.push_back({ nums[r_ptr], r_ptr });
-
-            if (r_ptr - l_ptr + 1 == k) {
-                while (!mono_q.empty() && mono_q.front().second <= r_ptr - k) {
-                    mono_q.pop_front();
-                }
-                res.push_back(mono_q.front().first);
-                l_ptr++;
-            }
-        }
-        return res;
-    }
-};
+// Last updated: 3/29/2026, 2:52:48 PM
+1/* 
+2Just use a multiset?
+3*/
+4class Solution {
+5public:
+6    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+7        int n = nums.size();
+8        multiset<int> window;
+9        for (int i = 0; i < k; i++) {
+10            window.insert(nums[i]);
+11        }
+12
+13        vector<int> res = { *prev(window.end()) };
+14
+15        for (int i = k; i < n; i++) {
+16            window.erase(window.find(nums[i-k]));
+17            window.insert(nums[i]);
+18            res.push_back(*prev(window.end()));
+19        } 
+20
+21        return res;
+22    }
+23};
