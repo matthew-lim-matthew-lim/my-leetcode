9a29c55a7acc1359fd775840b4323796556cb2ea
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-04-05 22:51:15 +1000

    File updated at 4/5/2026, 10:51:15 PM

--- 2424-LongestUploadedPrefix/2424-LongestUploadedPrefix.cpp
+++ 2424-LongestUploadedPrefix/2424-LongestUploadedPrefix.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/5/2026, 10:50:53 PM
+// Last updated: 4/5/2026, 10:51:15 PM
 1/* 
 2- LL
 3Can do it with a LL. 
@@ -8,43 +8,44 @@
 7
 8- O(1) longest() query
 9- O(1) upload() query
-10
-11- Vector
-12Can do it with a vector. 
-13When we add a new entry, we put it in the vector.
-14If the current entry pointed to by the vector is used, bump the pointer.
-15The longest() query returns `pointer`. 
-16
-17The vector solution is better. 
-18- O(1) longest() query. 
-19- O(n) upload() query, when the user uploads something, it might cause the pointer
-20to move rightwards up to `n` times. 
-21 */
-22class LUPrefix {
-23public:
-24    LUPrefix(int n) {
-25        for (int i = 0; i <= n; i++) {
-26            data_.push_back(i);
-27            data_map_[i+1] = prev(data_.end());
-28        }
-29    }
-30    
-31    void upload(int video) {
-32        data_.erase(data_map_[video]);
-33        data_map_.erase(video);
-34    }
-35    
-36    int longest() {
-37        return data_.front();
-38    }
-39private:
-40    list<int> data_;
-41    unordered_map<int, list<int>::iterator> data_map_;
-42};
-43
-44/**
-45 * Your LUPrefix object will be instantiated and called as such:
-46 * LUPrefix* obj = new LUPrefix(n);
-47 * obj->upload(video);
-48 * int param_2 = obj->longest();
-49 */
+10It works, but way slower than vector due to poor cache locality.
+11
+12- Vector
+13Can do it with a vector. 
+14When we add a new entry, we put it in the vector.
+15If the current entry pointed to by the vector is used, bump the pointer.
+16The longest() query returns `pointer`. 
+17
+18The vector solution is better. 
+19- O(1) longest() query. 
+20- O(n) upload() query, when the user uploads something, it might cause the pointer
+21to move rightwards up to `n` times. 
+22 */
+23class LUPrefix {
+24public:
+25    LUPrefix(int n) {
+26        for (int i = 0; i <= n; i++) {
+27            data_.push_back(i);
+28            data_map_[i+1] = prev(data_.end());
+29        }
+30    }
+31    
+32    void upload(int video) {
+33        data_.erase(data_map_[video]);
+34        data_map_.erase(video);
+35    }
+36    
+37    int longest() {
+38        return data_.front();
+39    }
+40private:
+41    list<int> data_;
+42    unordered_map<int, list<int>::iterator> data_map_;
+43};
+44
+45/**
+46 * Your LUPrefix object will be instantiated and called as such:
+47 * LUPrefix* obj = new LUPrefix(n);
+48 * obj->upload(video);
+49 * int param_2 = obj->longest();
+50 */
