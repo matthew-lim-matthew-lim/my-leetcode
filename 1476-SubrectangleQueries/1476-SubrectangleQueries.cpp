47aa5da80819c580b352263d70e27bc0d85e5855
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-04-10 14:09:30 +1000

    File updated at 4/10/2026, 2:09:30 PM

--- 1476-SubrectangleQueries/1476-SubrectangleQueries.cpp
+++ 1476-SubrectangleQueries/1476-SubrectangleQueries.cpp
@@ -1,33 +1,41 @@
-// Last updated: 4/10/2026, 2:05:24 PM
-1/* 
-2Brute force surely works. 
-3 */
-4
-5class SubrectangleQueries {
-6public:
-7    SubrectangleQueries(vector<vector<int>>& rectangle) : 
-8        rec_(rectangle) 
-9    {}
-10    
-11    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-12        for (int i = row1; i <= row2; i++) {
-13            for (int j = col1; j <= col2; j++) {
-14                rec_[i][j] = newValue;
-15            }
-16        }
-17    }
-18    
-19    int getValue(int row, int col) {
-20        return rec_[row][col];
-21    }
-22
-23private:
-24    vector<vector<int>> rec_;
-25};
-26
-27/**
-28 * Your SubrectangleQueries object will be instantiated and called as such:
-29 * SubrectangleQueries* obj = new SubrectangleQueries(rectangle);
-30 * obj->updateSubrectangle(row1,col1,row2,col2,newValue);
-31 * int param_2 = obj->getValue(row,col);
-32 */
+// Last updated: 4/10/2026, 2:09:30 PM
+#include <vector>
+
+using namespace std;
+
+static const int _ = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class SubrectangleQueries {
+    vector<vector<int>> rect;
+    // Храним историю обновлений: {r1, c1, r2, c2, val}
+    struct Update {
+        int r1, c1, r2, c2, val;
+    };
+    vector<Update> updates;
+
+public:
+    SubrectangleQueries(vector<vector<int>>& rectangle) {
+        rect = rectangle;
+    }
+    
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        // Просто пушим в историю. O(1)
+        updates.push_back({row1, col1, row2, col2, newValue});
+    }
+    
+    int getValue(int row, int col) {
+        // Проверяем историю с конца (самые свежие данные)
+        for (int i = updates.size() - 1; i >= 0; --i) {
+            const auto& u = updates[i];
+            if (row >= u.r1 && row <= u.r2 && col >= u.c1 && col <= u.c2) {
+                return u.val;
+            }
+        }
+        // Если обновлений для этой точки не было, берем оригинал
+        return rect[row][col];
+    }
+};
