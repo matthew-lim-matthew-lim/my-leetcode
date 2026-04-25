7208f52e9a1df5ec32de752a3583c8d8573f8d6e
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-04-25 17:21:07 +1000

    File updated at 4/25/2026, 5:21:06 PM

--- 1865-FindingPairsWithaCertainSum/1865-FindingPairsWithaCertainSum.cpp
+++ 1865-FindingPairsWithaCertainSum/1865-FindingPairsWithaCertainSum.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/25/2026, 5:15:10 PM
+// Last updated: 4/25/2026, 5:21:06 PM
 1/* 
 2unordered_map<int nums1Val, int count> nums1Count;
 3- Use this to bulk update the number of totals, since each key
@@ -12,48 +12,42 @@
 11 */
 12class FindSumPairs {
 13private:
-14    unordered_map<int, int> nums1Count_;
-15    unordered_map<int, int> totals_;
-16
-17    vector<int> nums2_;
-18public:
-19    FindSumPairs(vector<int>& nums1, vector<int>& nums2) : 
+14    unordered_map<int, int> nums2Count_;
+15    vector<int> nums1_;
+16    vector<int> nums2_;
+17public:
+18    FindSumPairs(vector<int>& nums1, vector<int>& nums2) : 
+19        nums1_(nums1),
 20        nums2_(nums2) 
 21    {
-22        for (int val : nums1) {
-23            nums1Count_[val]++;
+22        for (int val : nums2) {
+23            nums2Count_[val]++;
 24        }
-25
-26        for (int val : nums2_) {
-27            for (const auto &p : nums1Count_) {
-28                totals_[p.first + val] += p.second;
-29            }
-30        }
+25    }
+26    
+27    void add(int index, int val) {
+28        nums2Count_[nums2_[index]]--;
+29        nums2_[index] += val;
+30        nums2Count_[nums2_[index]]++;
 31    }
 32    
-33    void add(int index, int val) {
-34        for (const auto &p : nums1Count_) {
-35            totals_[p.first + nums2_[index]] -= p.second;
-36        }
-37
-38        nums2_[index] += val;
-39
-40        for (const auto &p : nums1Count_) {
-41            totals_[p.first + nums2_[index]] += p.second;
-42        }
-43    }
-44    
-45    int count(int tot) {
-46        if (totals_.contains(tot)) {
-47            return totals_[tot];
-48        }
-49        return 0;
-50    }
-51};
-52
-53/**
-54 * Your FindSumPairs object will be instantiated and called as such:
-55 * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
-56 * obj->add(index,val);
-57 * int param_2 = obj->count(tot);
-58 */
+33    int count(int tot) {
+34        int res = 0;
+35
+36        for (int val : nums1_) {
+37            int rem = tot - val;
+38            if (nums2Count_.contains(rem)) {
+39                res += nums2Count_[rem];
+40            }
+41        }
+42
+43        return res;
+44    }
+45};
+46
+47/**
+48 * Your FindSumPairs object will be instantiated and called as such:
+49 * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+50 * obj->add(index,val);
+51 * int param_2 = obj->count(tot);
+52 */
