c6217be76c59f8ce22a974f6ce4327fff673b919
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-04-25 20:00:30 +1000

    File updated at 4/25/2026, 8:00:29 PM

--- 743-NetworkDelayTime/743-NetworkDelayTime.cpp
+++ 743-NetworkDelayTime/743-NetworkDelayTime.cpp
@@ -1,55 +1,43 @@
-// Last updated: 11/30/2025, 7:24:31 PM
-1/* 
-2Seems like a simple Djikstras, except that Djikstras finds the shortest path, 
-3and we want to find the longest path. 
-4We can simply treat each edge like (6000 - weight). That is, shortest edges
-5are less favourable. 
-6Actually, that won't work, because if there is a more efficient path, the algorithm will take it. 
-7
-8The real solution would be to use BFS, and at each node keep track of the current time. We then only use the minimum time to each node. 
-9And we keep going until the queue is empty. 
-10If the queue is empty, and we visited all the nodes, we return an answer. 
+// Last updated: 4/25/2026, 8:00:29 PM
+1class Solution {
+2public:
+3    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+4        vector<vector<vector<int>>> adjList(n);
+5        for (const vector<int> &time : times) {
+6            adjList[time[0]-1].push_back({ time[1]-1, time[2] });
+7        }
+8
+9        vector<int> res(n, INT_MAX);
+10        res[k-1] = 0;
 11
-12Visit a neighbour if 'curr + edge < dist[neigh]`.
-13 */
+12        priority_queue<vector<int>> pq;
+13        pq.push({0, k-1});
 14
-15class Solution {
-16public:
-17    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-18        // Construct adjList
-19        vector<vector<pair<int, int>>> adjList(n + 1, vector<pair<int, int>>());
-20        for (vector<int> &time : times) {
-21            adjList[time[0]].push_back({ time[2], time[1]});
-22        }
+15        while (!pq.empty()) {
+16            int d = pq.top()[0];
+17            int u = pq.top()[1];
+18            pq.pop();
+19
+20            if (res[u] < d) {
+21                continue;
+22            }
 23
-24        // Algo
-25        vector<int> dist(n + 1, INT_MAX);
-26        dist[0] = 0;
-27        dist[k] = 0;
-28        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-29
-30        pq.push({0, k});
-31
-32        while (!pq.empty()) {
-33            auto [ d, u ] = pq.top();
-34            pq.pop();
-35
-36            for (pair<int, int> &e : adjList[u]) {
-37                int w = e.first;
-38                int v = e.second;
+24            for (const vector<int> &neigh : adjList[u]) {
+25                int v = neigh[0];
+26                int w = neigh[1];
+27
+28                if (res[v] > res[u] + w) {
+29                    res[v] = res[u] + w;
+30                    pq.push({
+31                        res[u] + w,
+32                        v
+33                    });
+34                }
+35            }
+36        }
+37
+38        int finalRes = *max_element(res.begin(), res.end());
 39
-40                if (dist[u] + w < dist[v]) {
-41                    dist[v] = dist[u] + w;
-42                    pq.push({dist[v], v});
-43                }
-44            }
-45        }
-46
-47        // Return the longest distance
-48        if (find(dist.begin(), dist.end(), INT_MAX) != dist.end()) {
-49            return -1;
-50        }
-51
-52        return *max_element(dist.begin(), dist.end());
-53    }
-54};
+40        return finalRes == INT_MAX ? -1 : finalRes;
+41    }
+42};
