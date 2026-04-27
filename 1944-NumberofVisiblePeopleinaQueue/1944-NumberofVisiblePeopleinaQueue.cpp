eeeec264c9b3cb46284b138ad17df9f259fa7fb3
Matthew Lim <matthewlimcenterdiv@gmail.com>
2026-04-27 15:44:37 +1000

    File updated at 4/27/2026, 3:44:36 PM

--- 1944-NumberofVisiblePeopleinaQueue/1944-NumberofVisiblePeopleinaQueue.cpp
+++ 1944-NumberofVisiblePeopleinaQueue/1944-NumberofVisiblePeopleinaQueue.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/27/2026, 3:44:03 PM
+// Last updated: 4/27/2026, 3:44:36 PM
 1/* 
 2- This is Shorter: res[i] = 1. Push this one. 
 3- This is Taller: res[i] = stk.size(). Pop all shorter from the stack, and push this one. 
@@ -19,23 +19,18 @@
 18                continue;
 19            }
 20
-21            if (heights[i] < stk.top()) {
-22                res[i] = 1;
-23                stk.push(heights[i]);
-24            } else {
-25                int popCount = 0;
-26                while (!stk.empty() && stk.top() <= heights[i]) {
-27                    stk.pop();
-28                    popCount++;
-29                }
-30                if (!stk.empty()) {
-31                    popCount++;
-32                }
-33                res[i] = popCount;
-34                stk.push(heights[i]);
-35            }
-36        }
-37
-38        return res;
-39    }
-40};
+21            int popCount = 0;
+22            while (!stk.empty() && stk.top() <= heights[i]) {
+23                stk.pop();
+24                popCount++;
+25            }
+26            if (!stk.empty()) {
+27                popCount++;
+28            }
+29            res[i] = popCount;
+30            stk.push(heights[i]);
+31        }
+32
+33        return res;
+34    }
+35};
